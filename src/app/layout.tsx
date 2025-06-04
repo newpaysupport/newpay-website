5967caa51b35696d35741cb938b45ad8d6b9589b
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-04 22:21:34 +0700

    init project
    add folder components
    add font.ts
    setup projet

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from 'react';
+
+import type { Metadata } from 'next';
+
+import { inter } from '@/app/fonts/fonts';
+import '@/app/globals.css';
+
+export const metadata: Metadata = {
+    title: 'NewPay',
+    description: 'NewPay'
+};
+
+const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
+    return (
+        <html suppressHydrationWarning lang='en'>
+            <body className={`${inter.variable} font-sans`}>{children}</body>
+        </html>
+    );
+};
+
+export default Layout;
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * The main page component that renders the HomePage component.
+ *
+ * @returns {JSX.Element} The rendered HomePage component.
+ */
+const Page = () => {
+    return <div className="text-center text-5xl">NewPay</div>;
+};
+
+export default Page;
