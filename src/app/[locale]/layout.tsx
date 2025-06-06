533a463eb81f0da0e9f4c9c1ea2ac02113773eae
tin.nd <tinn948@gmail.com>
2025-06-06 19:53:54 +0700

    fix: add promise locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,10 +8,10 @@ export default async function LocaleLayout({
     params
 }: {
     children: React.ReactNode;
-    params: { locale: string };
+    params: Promise<{ locale: string }>;
 }) {
     // Ensure that the incoming `locale` is valid
-    const { locale } = params;
+    const { locale } = await params;
     if (!hasLocale(routing.locales, locale)) {
         notFound();
     }
