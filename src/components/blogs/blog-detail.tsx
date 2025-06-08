695819b2bffc3d172cac45fbaf96fa0cef3a3561
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-08 23:01:34 +0700

    fix : blog-detail.tsx

--- src/components/blogs/blog-detail.tsx
+++ src/components/blogs/blog-detail.tsx
@@ -1,9 +1,10 @@
 'use client';
-import Image from 'next/image';
 import Card from '@/components/blogs/card';
-import { blog as enBlog } from '@/i18n/messages/en.json';
+import { blogSections, blog as enBlog } from '@/i18n/messages/en.json';
 import { blog as ziBlog } from '@/i18n/messages/zi.json';
+import Image from 'next/image';
 
+import { useLocale, useTranslations } from 'next-intl';
 import { FaArrowLeft, FaFacebookF, FaRedditAlien, FaXTwitter } from 'react-icons/fa6';
 import { HiOutlineCalendarDateRange } from 'react-icons/hi2';
 import { LuLink } from 'react-icons/lu';
@@ -11,39 +12,38 @@ import { MdOutlineTimer } from 'react-icons/md';
 import { PiTelegramLogoBold } from 'react-icons/pi';
 import { SiSinaweibo } from 'react-icons/si';
 import logoAuthor from '../../../public/Author.png';
-import { useLocale, useTranslations } from 'next-intl';
 
-const blogSections = [
-    {
-        title: 'Facing Volatility with Innovation',
-        paragraphs: [
-            `In much of Latin America, managing money often means navigating uncertainty. From currency volatility, high transaction fees and limited access to global financial services, individuals and small businesses face daily challenges in maintaining economic stability.`,
-            `In this context, crypto is gaining traction not as a trend, but as a tool. Across the region, people are adopting cryptocurrencies, especially stablecoins and digital payment solutions to solve real problems—preserving value during times of volatility, receiving payments from abroad, and accessing digital finance without relying on traditional banking systems.`,
-            `This shift is especially noticeable in cross-border payments. Traditional channels can be costly and slow, often involving multiple intermediaries. In contrast, crypto offers near-instant transfers, lower fees, and greater accessibility—even for those without a formal bank account.`,
-            `What we’re seeing in Latin America is not a speculative rush, but a pragmatic transition: from digital assets to real-world utility. From holding crypto to actively using them in daily life. And from barriers to empowerment—powered by technology, and catered to local needs.`,
-        ],
-    },
-    {
-        title: 'Why Crypto Adoption in LATAM Is Different',
-        paragraphs: [
-            `Crypto adoption in Latin America is shaped by real-world needs rather than market trends. Unlike regions where crypto is often viewed through the lens of speculation or investment, in LATAM, it serves a more grounded function: offering financial tools where traditional systems fall short.`,
-        ],
-        bullets: [
-            'In countries with volatile exchange rates, crypto offers a more stable alternative for cross-border payments.',
-            'Freelancers and remote workers use stablecoins to receive payments from clients abroad—often faster and with lower fees than traditional methods.',
-            'Small businesses adopt crypto to access a broader customer base and reduce exposure to local currency fluctuations.',
-            'For many without access to conventional banking, crypto wallets offer a first entry point into global commerce.',
-        ],
-        paragraphsAfterBullets: [
-            `In this context, crypto isn’t just a new financial asset—it’s a practical solution for navigating regional challenges. Its adoption continues to grow, not through hype, but through daily utility.`,
-        ],
-    },
-];
+// const blogSections = [
+//     {
+//         title: 'Facing Volatility with Innovation',
+//         paragraphs: [
+//             `In much of Latin America, managing money often means navigating uncertainty. From currency volatility, high transaction fees and limited access to global financial services, individuals and small businesses face daily challenges in maintaining economic stability.`,
+//             `In this context, crypto is gaining traction not as a trend, but as a tool. Across the region, people are adopting cryptocurrencies, especially stablecoins and digital payment solutions to solve real problems—preserving value during times of volatility, receiving payments from abroad, and accessing digital finance without relying on traditional banking systems.`,
+//             `This shift is especially noticeable in cross-border payments. Traditional channels can be costly and slow, often involving multiple intermediaries. In contrast, crypto offers near-instant transfers, lower fees, and greater accessibility—even for those without a formal bank account.`,
+//             `What we’re seeing in Latin America is not a speculative rush, but a pragmatic transition: from digital assets to real-world utility. From holding crypto to actively using them in daily life. And from barriers to empowerment—powered by technology, and catered to local needs.`,
+//         ],
+//     },
+//     {
+//         title: 'Why Crypto Adoption in LATAM Is Different',
+//         paragraphs: [
+//             `Crypto adoption in Latin America is shaped by real-world needs rather than market trends. Unlike regions where crypto is often viewed through the lens of speculation or investment, in LATAM, it serves a more grounded function: offering financial tools where traditional systems fall short.`,
+//         ],
+//         bullets: [
+//             'In countries with volatile exchange rates, crypto offers a more stable alternative for cross-border payments.',
+//             'Freelancers and remote workers use stablecoins to receive payments from clients abroad—often faster and with lower fees than traditional methods.',
+//             'Small businesses adopt crypto to access a broader customer base and reduce exposure to local currency fluctuations.',
+//             'For many without access to conventional banking, crypto wallets offer a first entry point into global commerce.',
+//         ],
+//         paragraphsAfterBullets: [
+//             `In this context, crypto isn’t just a new financial asset—it’s a practical solution for navigating regional challenges. Its adoption continues to grow, not through hype, but through daily utility.`,
+//         ],
+//     },
+// ];
 
 
 const BlogDetail = () => {
     const locale = useLocale();
-    const t = useTranslations('blog');
+    const t = useTranslations('blogSections');
 
     const blogData = locale === 'en' ? enBlog : ziBlog;
     const listCardData = blogData.listCardData;
@@ -63,7 +63,11 @@ const BlogDetail = () => {
                     <div className="w-full">
                         <div>
                             <h1 className="text-2xl md:text-3xl font-semibold mb-6">
-                                Crypto in Latin America: From Digital Assets to Cross-Border Payments
+                                {
+                                    locale === 'en' ?
+                                        'Crypto in Latin America: From Digital Assets to Cross-Border Payments'
+                                        : '拉丁美洲的加密货币：从数字资产到跨境支付'
+                                }
                             </h1>
 
                             <div className="text-sm md:text-md flex flex-wrap gap-4 items-center text-gray-500 mb-8">
@@ -85,20 +89,20 @@ const BlogDetail = () => {
 
                     {blogSections.map((section, index) => (
                         <div key={index} className="mb-12">
-                            <h2 className="font-semibold text-lg md:text-xl mb-4">{section.title}</h2>
+                            <h2 className="font-semibold text-lg md:text-xl mb-4">{t(`${index}.title`)}</h2>
                             <div className="text-sm md:text-base flex flex-col gap-4 text-justify">
                                 {section.paragraphs?.map((text, idx) => (
-                                    <p key={idx}>{text}</p>
+                                    <p key={idx}>{t(`${index}.paragraphs.${idx}`)}</p>
                                 ))}
                                 {section.bullets && (
                                     <ul className="list-disc pl-5 flex flex-col gap-3">
                                         {section.bullets.map((item, i) => (
-                                            <li key={i}>{item}</li>
+                                            <li key={i}>{t(`${index}.bullets.${i}`)}</li>
                                         ))}
                                     </ul>
                                 )}
                                 {section.paragraphsAfterBullets?.map((text, idx) => (
-                                    <p key={`after-${idx}`}>{text}</p>
+                                    <p key={`after-${idx}`}>{t(`${index}.paragraphsAfterBullets.${idx}`)}</p>
                                 ))}
                             </div>
                         </div>
@@ -123,11 +127,12 @@ const BlogDetail = () => {
 
             {/* Related Posts */}
             <div className="mt-16">
-                <h2 className="text-2xl md:text-3xl font-semibold mb-6">Further Reading</h2>
+                <h2 className="text-2xl md:text-3xl font-semibold mb-6">{locale == 'en' ? "Further Reading" : '进一步阅读'}</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                     {sliceCard.map((card) => (
                         <Card
                             key={card.id}
+                            id={card.id}
                             title={card.title}
                             date={card.date}
                             readTime={card.readTime}
