fc4fb627a8305e3ae57f016b30fc4926813ccfb6
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-08 23:39:03 +0700

    fix :  card.tsx

--- src/components/blogs/card.tsx
+++ src/components/blogs/card.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image, { StaticImageData } from 'next/image';
-import { useRouter, useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { FaLink } from 'react-icons/fa';
 
 type CardProps = {
