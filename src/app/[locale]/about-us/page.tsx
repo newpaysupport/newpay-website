f523eb9daf69286ae4907a06dee6dc8e48c3a9a3
tin.nd <tin.nd@vgamestd.com>
2025-06-11 17:11:37 +0700

    feat: organize component about-us

---
+++ src/app/[locale]/about-us/page.tsx
@@ -0,0 +1,16 @@
+import Compliance from '@/components/about-us/compliance'
+import Hero from '@/components/about-us/hero'
+import Insight from '@/components/about-us/insight'
+import React from 'react'
+
+const AboutUs = () => {
+    return (
+        <main>
+            <Hero />
+            <Insight />
+            <Compliance />
+        </main>
+    )
+}
+
+export default AboutUs
