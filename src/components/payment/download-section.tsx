45bf1e6bf88119a546f94c676e04a423668d3a23
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-12 21:50:06 +0700

    fix : payment\download-section.tsx

--- src/components/payment/download-section.tsx
+++ src/components/payment/download-section.tsx
@@ -13,21 +13,25 @@ const DownloadSection = ({ downloadSection }: DownloadSectionProps) => {
     return (
         <>
             {/* content Download section */}
-            <div className="bg-gray-600/10 rounded-4xl py-16">
-                <div className="flex md:flex-row flex-col container mx-auto gap-20 justify-center items-center">
-                    <Image src={product_content_end} alt="Gift Icon" width={540} height={540} />
-                    <div className='md:p-0 p-6'>
-                        <Image src={logo_payment} alt="Gift Icon" width={100} height={100} className="w-16 h-16 md:w-20 md:h-20" />
-                        <h1 className="md:w-[70%] w-full md:text-5xl md:leading-14 text-2xl font-semibold pt-4 lg:pt-10 text-left text-white">
-                            {downloadSection.title}
-                        </h1>
-                        <button
-                            className="md:text-md text-sm rounded-xl mt-6 md:py-3 md:px-4 md:text-sm p-4 text-black cursor-pointer bg-white hover:bg-gray-100 font-semibold">
-                            {downloadSection.buttonText}
-                        </button>
-                    </div>
+            <div style={{
+                borderRadius: '80px',
+                background: 'rgba(255, 255, 255, 0.04));'
+            }}
+            className="bg-gray-600/10 rounded-4xl py-16">
+            <div className="flex md:flex-row flex-col container mx-auto gap-20 justify-center items-center">
+                <Image src={product_content_end} alt="Gift Icon" width={540} height={540} />
+                <div className='md:p-0 p-6'>
+                    <Image src={logo_payment} alt="Gift Icon" width={100} height={100} className="w-16 h-16 md:w-20 md:h-20" />
+                    <h1 className="md:w-[70%] w-full md:text-5xl md:leading-14 text-2xl font-semibold pt-4 lg:pt-10 text-left text-white">
+                        {downloadSection.title}
+                    </h1>
+                    <button
+                        className="md:text-md text-sm rounded-xl mt-6 md:py-3 md:px-4 md:text-sm p-4 text-black cursor-pointer bg-white hover:bg-gray-100 font-semibold">
+                        {downloadSection.buttonText}
+                    </button>
                 </div>
             </div>
+        </div >
         </>
     )
 }
