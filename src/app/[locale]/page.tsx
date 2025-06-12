39f96bb5e87ff9059675a1718a21d5dae0a613e4
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-12 21:47:42 +0700

    feat : add GiftCard in page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,17 @@
 
 import Consume from "@/components/home/consume";
+import GiftCard from "@/components/home/gift-card";
 import Hero from "@/components/home/hero";
 import OpenAccount from "@/components/home/open-account";
 
-
 export default function HomePage() {
+
     return (
         <main>
             <Hero />
             <Consume />
             <OpenAccount />
+            <GiftCard/>
         </main>
     );
 }
