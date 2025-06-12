94ca5c5b093f5ecb42a8c17fa5356552d0265d39
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-12 21:50:20 +0700

    fix : payment\index.tsx

--- src/components/payment/index.tsx
+++ src/components/payment/index.tsx
@@ -16,12 +16,12 @@ import icon_arrow from "@/images/payment/didi_arrow.png";
 import gift from "@/images/payment/gift.svg";
 import hero_payment from "@/images/payment/hero_payment.png";
 import img_share from "@/images/payment/img_share_happy.png";
-import message from "@/images/payment/message.png";
 import logo_payment from "@/images/payment/newpay_square_mark_payment.png";
 import scan_icon from "@/images/payment/scan.svg";
 
 import Image from "next/image";
 import DownloadSection from './download-section';
+import Faq from './faq';
 import PhysicalCard from "./physical-card";
 import VirtualCard from "./virtual-card";
 
@@ -77,7 +77,14 @@ const PaymentScreen = () => {
                 </div>
                 <div className="absolute md:w-[25%] w-[50%] top-[30%] bottom-[60%] left-[12%]">
                     <Image src={logo_payment} alt="background" width={0} height={0} className="w-12 h-12 mb-10" />
-                    <h1 className="md:text-5xl md:my-6 md:leading-14 text-xl my-4 text-white leading-8">{heroSection.title}</h1>
+                    <h1 style={{
+                        background: "linear-gradient(90deg, #FFF 0.13%, rgba(255, 255, 255, 0.00) 128.16%)",
+                        backgroundClip: 'text',
+                        WebkitBackgroundClip: 'text',
+                        WebkitTextFillColor: "transparent"
+                    }} className="md:text-5xl md:my-6 md:leading-14 text-xl my-4 text-white leading-8">
+                        {heroSection.title}
+                    </h1>
                     <button className="rounded-full md:py-3 md:px-4 md:text-sm p-2 text-xs text-white cursor-pointer bg-orange-500 hover:bg-orange-600">{heroSection.buttonText}</button>
                 </div>
             </div>
@@ -202,36 +209,7 @@ const PaymentScreen = () => {
                     </div>
                 </div>
                 {/* content FAQ */}
-                <div className="mt-16 md:pb-6 md:p-0 p-6">
-                    <div className="flex md:flex-row flex-col mx-auto max-w-7xl justify-between">
-                        <div className="text-white">
-                            <h1 className="text-6xl font-semibold">
-                                {faqSection.title}
-                            </h1>
-                            <p className="text-md mt-3">
-                                {faqSection.subtitle}
-                            </p>
-                            <Image src={message} alt="Message Icon" width={205} height={180} />
-                        </div>
-                        <div className="text-white">
-                            {faqItems.map((item, index) => {
-                                return (
-                                    <details key={item.question} className="border-b border-gray-800 py-6 group">
-                                        <summary className="md:w-[640px] w-full flex justify-between items-center text-left focus:outline-none cursor-pointer">
-                                            <h3 className="md:text-xl text-sm font-medium">{item.question}</h3>
-                                            <span className="text-3xl transition-transform duration-300 group-open:rotate-45">
-                                                +
-                                            </span>
-                                        </summary>
-                                        <p className="md:text-md md:w-[640px] w-full text-xs mt-4 text-gray-400 leading-6 font-medium">
-                                            {item.answer}
-                                        </p>
-                                    </details>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
+                <Faq faqSection={faqSection} faqItems={faqItems} />
                 {/* content Download section */}
                 <DownloadSection downloadSection={downloadSection} />
             </div>
