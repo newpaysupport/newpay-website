a64c3a3e0c9771b8323d46d6e6e28c553efd95c8
tin.nd <tinn948@gmail.com>
2025-06-15 00:01:47 +0700

    feat: hero component

---
+++ src/app/[locale]/secure/page.tsx
@@ -0,0 +1,15 @@
+import Hero from '@/components/secure-custodian/hero'
+import ReactLenis from 'lenis/react'
+import React from 'react'
+
+const SecureCustodian = () => {
+    return (
+        <ReactLenis>
+            <main>
+                <Hero />
+            </main>
+        </ReactLenis>
+    )
+}
+
+export default SecureCustodian
