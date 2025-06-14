7057de0e90e079cd6374db025b856b89d45f3747
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-14 14:20:39 +0700

    fix : rename CardSlide -> AutoSlideShow in card-slide.tsx

--- src/components/home/joint/card-slide.tsx
+++ src/components/home/joint/card-slide.tsx
@@ -6,6 +6,7 @@ import slide3 from '@/images/home/joint/slide3.png';
 import slide4 from '@/images/home/joint/slide4.png';
 import slide5 from '@/images/home/joint/slide5.png';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const slides = [
     { title: 'INTERACTIVE MEDIA', image: slide1 },
@@ -16,13 +17,23 @@ const slides = [
 ];
 
 const AutoSlideshow = () => {
+    const [current, setCurrent] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrent((prev) => (prev + 1) % slides.length);
+        }, 3000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className="py-20 w-full max-w-full overflow-hidden bg-[#FFF]">
+        <div className="py-20 w-full overflow-hidden bg-white">
             <h1 className="text-4xl md:text-6xl text-[#1B1B1B] font-semibold max-w-7xl mx-auto text-left">
                 Industry Coverage
             </h1>
-            <div className="slideshow-container relative w-full overflow-hidden mt-20 mx-auto">
 
+            <div className="relative mt-30 mb-10 flex justify-center items-center">
+                {/* overlay  */}
                 <div style={{
                     background: "linear-gradient(90deg, rgba(255, 255, 255, 1) 5%, rgba(255, 255, 255, 0) 100%)",
                 }} className='absolute w-[120px] h-[450px] left-0 bottom-0 z-50'></div>
@@ -30,19 +41,24 @@ const AutoSlideshow = () => {
                     background: "linear-gradient(90deg, rgba(255, 255, 255, 0.00) 5%, rgba(255, 255, 255, 1) 100%)"
                 }} className='absolute w-[120px] h-[450px] right-0 bottom-0 z-50'></div>
 
-                <div className="slideshow-track flex w-[calc((400px+30px)*10)] animate-slide">
-                    {slides.concat(slides).map((slide, index) => (
+                {[-2, -1, 0, 1, 2].map((offset, i) => {
+                    const index = (current + offset + slides.length) % slides.length;
+                    //apply style index
+                    let style = 'scale-90 opacity-60 z-0';
+                    if (offset === 0) style = 'scale-120 opacity-100 z-30 mx-10';
+                    else if (Math.abs(offset) === 1) style = 'scale--100 opacity-90 z-20 ';
+                    else if (Math.abs(offset) === 2) style = 'scale-90 opacity-80 z-10 ';
+                    return (
                         <div
-                            key={index}
-                            className="slide flex-none relative mr-[30px] md:w-[330px] md:h-[410px] w-[200px] h-[183px]"
+                            key={i}
+                            className={`transition-all duration-700 ease-in-out transform  ${style} md:w-[330px] md:h-[410px] rounded-3xl relative overflow-hidden`}
                         >
                             <Image
-                                src={slide.image}
-                                alt={slide.title}
-                                className="w-full h-full object-cover rounded-3xl"
+                                src={slides[index].image}
+                                alt={slides[index].title}
                                 width={330}
-                                height={412}
-                                priority={index === 0}
+                                height={410}
+                                className="w-full h-full object-cover rounded-3xl"
                             />
                             <div className="absolute bottom-1 left-1 text-white p-2">
                                 <h3
@@ -51,14 +67,14 @@ const AutoSlideshow = () => {
                                             'linear-gradient(0deg, rgba(174, 174, 174, 0.32) 0%, rgba(174, 174, 174, 0.32) 100%), rgba(255, 255, 255, 0.08)',
                                         backdropFilter: 'blur(20px)',
                                     }}
-                                    className="text-md font-semibold py-2 px-4 rounded-full uppercase"
+                                    className="text-sm font-semibold py-2 px-4 rounded-full uppercase"
                                 >
-                                    {slide.title}
+                                    {slides[index].title}
                                 </h3>
                             </div>
                         </div>
-                    ))}
-                </div>
+                    );
+                })}
             </div>
         </div>
     );
