9f06d70024a0f4d819287201438be02afe0ca2a5
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-15 14:42:20 +0700

    fix : globe-listcard.tsx

--- src/components/home/gift-card/globe-listcard.tsx
+++ src/components/home/gift-card/globe-listcard.tsx
@@ -26,7 +26,7 @@ const GlobeListCard = () => {
                             style = "rotate(8deg)";
                         }
                         return (
-                            <div className='relative overflow-hidden group hover:-translate-y-5 transition-all ease-linear duration-500'>
+                            <div className='relative overflow-hidden group hover:-translate-y-5 transition-all ease-linear duration-500 cursor-pointer'>
                                 <Image src={item.image} width={304} height={0} alt="Globe Image" className="object-cover w-76" />
                                 <div style={{
                                     transform: `${style}`,
