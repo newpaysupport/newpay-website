581a0056d89c0f2ac30e326f0703f602a3868c52
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-16 01:02:17 +0700

    fix : position card

--- src/components/home/gift-card/globe-listcard.tsx
+++ src/components/home/gift-card/globe-listcard.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import globe from '@/images/home/giftcard/Globe.png';
-import shadow_globe from '@/images/home/giftcard/shadow_globe.png';
 import { home as en } from '@/i18n/messages/en.json';
 import { home as zi } from '@/i18n/messages/zi.json';
-import Image from 'next/image';
+import globe from '@/images/home/giftcard/Globe.png';
+import shadow_globe from '@/images/home/giftcard/shadow_globe.png';
 import { useLocale } from 'next-intl';
+import Image from 'next/image';
 
 const GlobeListCard = () => {
     const locale = useLocale()
@@ -16,7 +16,7 @@ const GlobeListCard = () => {
             <div className='pt-60'>
                 <Image src={globe} alt="Globe Image" className="w-full object-cover" />
 
-                <div className='absolute right-0 left-0 bottom-0 flex gap-5 items-start justify-center top-60'>
+                <div className='absolute right-0 left-0 top-45 bottom-0 flex gap-5 items-start justify-center '>
 
                     {globeListCard.map((item, index) => {
                         let style = "rotate(0deg)";
@@ -26,17 +26,18 @@ const GlobeListCard = () => {
                             style = "rotate(8deg)";
                         }
                         return (
-                            <div className='relative overflow-hidden group hover:-translate-y-5 transition-all ease-linear duration-500 cursor-pointer'>
-                                <Image src={item.image} width={304} height={0} alt="Globe Image" className="object-cover w-76" />
+                            <div key={item.title} className='relative overflow-hidden group hover:-translate-y-5 transition-all ease-linear duration-500 cursor-pointer'>
+                                <Image src={item.image} width={300} height={300} alt="Globe Image" className="object-cover w-76" />
                                 <div style={{
                                     transform: `${style}`,
+
                                 }}
                                     className={`absolute right-[18px] left-[20px]
                                     ${index === 3 || index === 0 ? 'bottom-12' : 'bottom-10'}
                                      transition-all duration-500 ease-in-out`}>
                                     <h2 className={`
-                                    text-white text-xl font-semibold my-2
-                                    ${index === 3 || index === 0 ? '' : 'transform translate-y-18'} transform translate-y-10 transition-all duration-500 ease-in-out
+                                    text-white text-xl font-medium my-2
+                                    ${index === 3 || index === 0 ? '' : 'transform translate-y-10'} transform translate-y-5 transition-all duration-500 ease-in-out
                                     group-hover:translate-y-0
                                     `}>
                                         {item.title}
