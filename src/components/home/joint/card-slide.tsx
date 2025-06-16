0a0def3d5a8a61f714b615b4a67c8d22b3654c9f
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-16 13:26:09 +0700

    fix : animation cardSlide

--- src/components/home/joint/card-slide.tsx
+++ src/components/home/joint/card-slide.tsx
@@ -8,10 +8,7 @@ import { useLocale } from 'next-intl';
 const AutoSlideshow = () => {
     const locale = useLocale();
     const joint = locale === 'en' ? enBlog : ziBlog;
-    const slides = joint.cardSlide.slides.map((slide) => ({
-        title: slide.title,
-        image: slide.image,
-    }));
+    const slides = joint.cardSlide.slides
     const [current, setCurrent] = useState(0);
 
     useEffect(() => {
@@ -22,54 +19,47 @@ const AutoSlideshow = () => {
     }, []);
 
     return (
-        <div className="py-20 w-full overflow-hidden bg-white">
+        <div className="py-20 w-full overflow-hidden bg-white relative">
             <h1 className="text-4xl md:text-6xl text-[#1B1B1B] font-semibold container mx-auto text-left">
                 {joint.cardSlide.title}
             </h1>
 
-            <div className="relative mt-30 mb-10 flex justify-center items-center">
-                {/* overlay  */}
-                <div style={{
+             {/* overlay  */}
+             <div style={{
                     background: "linear-gradient(90deg, rgba(255, 255, 255, 1) 5%, rgba(255, 255, 255, 0) 100%)",
-                }} className='absolute w-[120px] h-[450px] left-0 bottom-0 z-50'></div>
+                }} className='absolute w-[120px] h-[450px] left-0 bottom-[12%] z-50'></div>
                 <div style={{
                     background: "linear-gradient(90deg, rgba(255, 255, 255, 0.00) 5%, rgba(255, 255, 255, 1) 100%)"
-                }} className='absolute w-[120px] h-[450px] right-0 bottom-0 z-50'></div>
+                }} className='absolute w-[120px] h-[450px] right-0 bottom-[12%] z-50'></div>
 
-                {[-2, -1, 0, 1, 2].map((offset, i) => {
-                    const index = (current + offset + slides.length) % slides.length;
-                    //apply style index
-                    let style = 'scale-90 opacity-60 z-0';
-                    if (offset === 0) style = 'scale-120 opacity-100 z-30 mx-10';
-                    else if (Math.abs(offset) === 1) style = 'scale-100 opacity-90 z-20 ';
-                    else if (Math.abs(offset) === 2) style = 'scale-90 opacity-80 z-10 ';
-                    return (
-                        <div
-                            key={i}
-                            className={`transition-all duration-700 ease-in-out transform  ${style} md:w-[330px] md:h-[410px] rounded-3xl relative overflow-hidden`}
-                        >
-                            <Image
-                                src={slides[index].image}
-                                alt={slides[index].title}
-                                width={330}
-                                height={410}
-                                className="w-full h-full object-cover rounded-3xl"
-                            />
-                            <div className="absolute bottom-1 left-1 text-white p-2">
-                                <h3
-                                    style={{
-                                        background:
-                                            'linear-gradient(0deg, rgba(174, 174, 174, 0.32) 0%, rgba(174, 174, 174, 0.32) 100%), rgba(255, 255, 255, 0.08)',
-                                        backdropFilter: 'blur(20px)',
-                                    }}
-                                    className="text-sm font-semibold py-2 px-4 rounded-full uppercase"
-                                >
-                                    {slides[index].title}
-                                </h3>
-                            </div>
+            <div className="relative mt-30 mb-10 flex justify-center items-center animation-slideshow gap-5">
+
+                {[...slides, ...slides].map((slide, i) => (
+                    <div
+                        key={i}
+                        className="slide-item md:w-[330px] md:h-[410px] w-[200px] h-[300px] rounded-3xl relative overflow-hidden flex-shrink-0"
+                    >
+                        <Image
+                            src={slide.image}
+                            alt={slide.title}
+                            width={330}
+                            height={410}
+                            className="w-full h-full object-cover rounded-3xl"
+                        />
+                        <div className="absolute bottom-1 left-1 text-white p-2">
+                            <h3
+                                style={{
+                                    background:
+                                        'linear-gradient(0deg, rgba(174, 174, 174, 0.32) 0%, rgba(174, 174, 174, 0.32) 100%), rgba(255, 255, 255, 0.08)',
+                                    backdropFilter: 'blur(20px)',
+                                }}
+                                className="text-sm font-semibold py-2 px-4 rounded-full uppercase"
+                            >
+                                {slide.title}
+                            </h3>
                         </div>
-                    );
-                })}
+                    </div>
+                ))}
             </div>
         </div>
     );
