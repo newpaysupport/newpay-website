3ffd51e97ddddabc8af0a40e642c013420251b20
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-18 02:02:35 +0700

    feat : add animation for icon

--- src/components/payment/content-main.tsx
+++ src/components/payment/content-main.tsx
@@ -1,4 +1,4 @@
-import world from "@/images/payment/World.png";
+import world from "@/images/payment/content-main/world.png";
 
 import { payment as enBlog } from '@/i18n/messages/en.json';
 import { payment as ziBlog } from '@/i18n/messages/zi.json';
@@ -11,6 +11,11 @@ import netflix from "@/images/payment/content-main/netflix.svg";
 import spotify from "@/images/payment/content-main/spotify.svg";
 import tiktok from "@/images/payment/content-main/tiktok.svg";
 import youtube from "@/images/payment/content-main/youtube.svg";
+import bitcoin from "@/images/payment/content-main/bitcoin.svg";
+import eth from "@/images/payment/content-main/eth.svg";
+import solana from "@/images/payment/content-main/solana.svg";
+import tether from "@/images/payment/content-main/tether.svg";
+import usd from "@/images/payment/content-main/usd.svg";
 
 const ContentMain = () => {
     const locale = useLocale();
@@ -25,7 +30,34 @@ const ContentMain = () => {
         <div className="mt-30 container mx-auto overflow-hidden">
             <h1 className="md:text-6xl text-2xl font-semibold text-center pt-10">{contentSection2.title}</h1>
             <p className="md:text-md text-sm text-center text-gray-500 mt-4 mx-auto">{contentSection2.description}</p>
-            <Image src={world} alt="world" width={1000} height={500} className="mx-auto mt-10" />
+            <div className="mx-auto mt-10 relative">
+                <Image src={world} alt="world" className="mx-auto" />
+
+                <Image src={bitcoin} alt="bitcoin" style={{
+                    borderRadius: "999px",
+                    background: "#FFF",
+                }} className="animate-bitcoin absolute top-[14%] left-[26%]" />
+
+                <Image src={eth} alt="eth" className="animate-eth absolute top-[8%] left-[43%]" />
+
+                <Image src={usd} alt="usd" style={{
+                    borderRadius: "999px",
+                    background: "#FFF",
+                }} className="animate-usd absolute top-[18%] right-[28%]" />
+
+                <Image src={tether} alt="tether" style={{
+                    borderRadius: "999px",
+                    background: "#FFF",
+                }} className="animate-tether absolute top-[57%] left-[30%]" />
+
+
+                <Image src={solana} alt="solana" style={{
+                    borderRadius: "999px",
+                    background: "#FFF",
+                }} className="animate-solana absolute top-[52%] right-[22%]" />
+            </div>
+
+
             <div className="mx-auto mt-10 relative" >
                 <div className='flex gap-8 justify-center animation-slideshow'>
                     {[...listImage, ...listImage].map((image, index) => {
