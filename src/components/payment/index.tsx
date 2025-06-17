b3346cd5d71dcd327efe5442daee4b331c0c5cb4
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-17 18:42:05 +0700

    fix : payment/index.tsx

--- src/components/payment/index.tsx
+++ src/components/payment/index.tsx
@@ -5,17 +5,10 @@ import { useLocale } from 'next-intl';
 import { useState } from "react";
 
 import card_container from "@/images/payment/Card_Container.png";
-import card_payment from "@/images/payment/Front.png";
-import social from "@/images/payment/Social_Media.png";
-import wallet from "@/images/payment/WALLET.png";
-import world from "@/images/payment/World.png";
 import access_icon from "@/images/payment/access.svg";
 import bg_content3 from "@/images/payment/bg_content3.png";
-import icon_gift from "@/images/payment/didi.png";
-import icon_arrow from "@/images/payment/didi_arrow.png";
-import gift from "@/images/payment/gift.svg";
+
 import hero_payment from "@/images/payment/hero_payment.png";
-import img_share from "@/images/payment/img_share_happy.png";
 import logo_payment from "@/images/payment/newpay_square_mark_payment.png";
 import scan_icon from "@/images/payment/scan.svg";
 
@@ -24,6 +17,9 @@ import DownloadSection from './download-section';
 import Faq from './faq';
 import PhysicalCard from "./physical-card";
 import VirtualCard from "./virtual-card";
+import ContentMain from './content-main';
+import ContentEnd from './content-end';
+import ContentCard from './content-card';
 
 const PaymentScreen = () => {
     const [activeCard, setActiveCard] = useState(true);
@@ -41,23 +37,8 @@ const PaymentScreen = () => {
 
     {/* Content: Virtual Card */ }
 
-    {/* Content Section 2*/ }
-    const contentSection2 = payment.contentSection2;
-
     {/* Centered Content */ }
     const safeAssetsSection = payment.safeAssetsSection;
-
-    {/* Content Section: Card */ }
-    const cardSection = payment.cardSection;
-
-    {/* Content: Share Happy Moment */ }
-    const shareHappyMoment = payment.shareHappyMoment;
-
-    {/* Card Gift */ }
-    const giftCard = payment.shareHappyMoment.giftCard;
-
-    {/* Card Receive */ }
-    const receiveCard = payment.shareHappyMoment.receiveCard;
 
     {/* Content Section: FAQ */ }
     const faqSection = payment.faqSection;
@@ -113,13 +94,7 @@ const PaymentScreen = () => {
                         : <PhysicalCard physicalItems={physicalCard.physicalItems} labelButton={contentSection1.labelButton} />}
 
                     {/* Content Section 2*/}
-                    <div className="mt-30">
-                        <h1 className="md:text-6xl text-2xl font-semibold text-center pt-10">{contentSection2.title}</h1>
-                        <p className="md:text-md text-sm text-center text-gray-500 mt-4 mx-auto">{contentSection2.description}</p>
-                        <Image src={world} alt="world" width={1000} height={500} className="mx-auto mt-10" />
-                        <Image src={social} alt="social" width={1000} height={500} className="mx-auto mt-10" />
-                    </div>
-
+                    <ContentMain/>
                     {/* content background */}
                     <div className="relative w-full h-[370px] mt-10">
                         <Image src={bg_content3} alt="social" fill className="object-cover w-full h-full" quality={100} />
@@ -147,63 +122,9 @@ const PaymentScreen = () => {
                         </div>
                     </div>
                     {/* Content Section: Card */}
-                    <div className="mt-8 sm:mt-12 lg:mt-16">
-                        <Image src={card_payment} alt="card payment" sizes="100vw" className="mx-auto w-full max-w-md sm:max-w-lg my-4 sm:my-6" />
-                        <Image src={wallet} alt="Wallet" sizes="100vw" className="mx-auto w-full max-w-md sm:max-w-lg" />
-
-                        <div className="mt-8 sm:mt-12 lg:mt-16 text-center">
-                            <h1 className="md:text-6xl text-3xl font-semibold pt-4 sm:pt-6 lg:pt-10">
-                                {cardSection.title}
-                            </h1>
-                            <p className="text-gray-500 mt-2 sm:mt-4 mx-auto w-full sm:w-[50%] lg:w-[20%] text-sm sm:text-base">
-                                {cardSection.description}
-                            </p>
-                        </div>
-                    </div>
+                    <ContentCard/>
                     {/* Content: Share Happy Moment */}
-                    <div className="mt-8 sm:mt-12 lg:mt-16 flex flex-col md:flex-row mx-auto max-w-6xl items-center justify-between px-4 sm:px-6 lg:px-8">
-                        <div className="flex flex-col w-full md:w-[35%]">
-                            <Image src={gift} alt="Share Happy Moment" />
-
-                            <h1 className="text-5xl font-semibold pt-4 sm:pt-6 lg:pt-10 text-left">
-                                {shareHappyMoment.title}
-                            </h1>
-                            <p className="text-left text-gray-500 mt-2 sm:mt-4 w-full sm:w-[70%] text-sm sm:text-base">
-                                {shareHappyMoment.description}
-                            </p>
-                            <div className="flex items-center justify-between bg-orange-500 text-white rounded-2xl p-2 sm:p-3 md:p-4 shadow-md w-full max-w-xs mt-8 sm:mt-12 lg:mt-16">
-                                <div className="flex items-center space-x-2 sm:space-x-3">
-                                    <div className="flex-shrink-0">
-                                        <Image src={icon_gift} alt="Gift Icon" width={24} height={24} className="w-8 h-8 md:w-12 md:h-12" />
-                                    </div>
-                                    <div>
-                                        <p className="text-base sm:text-lg font-semibold">{giftCard.title}</p>
-                                        <p className="text-xs sm:text-sm">{giftCard.subtitle}</p>
-                                    </div>
-                                </div>
-                                <div className="text-right">
-                                    <p className="text-base sm:text-lg font-semibold">{giftCard.amount}</p>
-                                </div>
-                            </div>
-                            <div className="flex items-center justify-between bg-green-400 text-white rounded-2xl p-2 sm:p-3 md:p-4 shadow-md w-full max-w-xs mt-4 sm:mt-6 ml-0 sm:ml-6 lg:ml-20">
-                                <div className="flex items-center space-x-2 sm:space-x-3">
-                                    <div className="flex-shrink-0">
-                                        <Image src={icon_arrow} alt="Receive Icon" width={24} height={24} className="w-8 md:w-12 h-8 md:h-12" />
-                                    </div>
-                                    <div>
-                                        <p className="text-base sm:text-lg font-semibold">{receiveCard.title}</p>
-                                        <p className="text-xs sm:text-sm">{receiveCard.subtitle}</p>
-                                    </div>
-                                </div>
-                                <div className="text-right">
-                                    <p className="text-base sm:text-lg font-semibold">{receiveCard.amount}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full md:w-[50%] mt-8 md:mt-0">
-                            <Image src={img_share} alt="Share Happy Moment" width={0} height={0} sizes="100vw" className="mx-auto w-full max-w-md sm:max-w-lg" />
-                        </div>
-                    </div>
+                    <ContentEnd/>
                 </div>
                 {/* content FAQ */}
                 <div className='pt-20'>
