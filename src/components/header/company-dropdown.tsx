781174e69cfea1d15ae9587ca3c885dc1a2b062b
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-18 22:50:05 +0700

    fix : dropdown company

--- src/components/header/company-dropdown.tsx
+++ src/components/header/company-dropdown.tsx
@@ -11,23 +11,23 @@ type LinkItem = {
     }
     locale?: string;
     isBlogAndContact?: boolean;
+    isScrolled?: boolean;
 };
 
-const CompanyDropdown = ({ link, locale, isBlogAndContact }: LinkItem) => {
+const CompanyDropdown = ({ link, locale, isBlogAndContact, isScrolled }: LinkItem) => {
 
     const getHeaderStyles = () => {
         if (isBlogAndContact) {
             return {
                 background: "white",
                 color: "black",
-                borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
                 fontWeight: '600',
                 zIndex: 50
             };
         }
         return {
             background: "linear-gradient(0deg, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0.05) 100%), rgba(6, 6, 6, 0.60)",
-            backdropFilter: "blur(50px)"
+            backdropFilter: isScrolled ? "blur(50px)" : "blur(8px)",
         };
     }
 
@@ -53,16 +53,16 @@ const CompanyDropdown = ({ link, locale, isBlogAndContact }: LinkItem) => {
                             }
                             return (
                                 <Link
-                                key={index}
-                                href={item.href}
-                                className={`block px-6 py-2 rounded-full transition-all ${hover} group`}
-                            >
-                                <div className="flex items-start gap-3">
-                                    <div className={`${style} text-sm  transition-colors`}>
-                                        {item.title}
+                                    key={index}
+                                    href={item.href}
+                                    className={`block px-6 py-2 rounded-full transition-all ${hover} group`}
+                                >
+                                    <div className="flex items-start gap-3">
+                                        <div className={`${style} text-sm  transition-colors`}>
+                                            {item.title}
+                                        </div>
                                     </div>
-                                </div>
-                            </Link>
+                                </Link>
                             )
                         })}
                     </div>
