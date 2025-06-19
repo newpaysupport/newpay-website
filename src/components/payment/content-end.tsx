b483fac8df6462ccc8e49426cbee632a9c2b9ae2
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-19 22:23:12 +0700

    feat : add animation title

--- src/components/payment/content-end.tsx
+++ src/components/payment/content-end.tsx
@@ -3,13 +3,14 @@ import { payment as ziBlog } from '@/i18n/messages/zi.json';
 import { useLocale } from 'next-intl';
 import Image from 'next/image';
 
+import img_bottom_left from "@/images/payment/content_end/img_bottom_left.png";
+import img_center from "@/images/payment/content_end/img_center.png";
+import img_top_right from "@/images/payment/content_end/img_top_right.png";
 import icon_gift from "@/images/payment/didi.png";
 import icon_arrow from "@/images/payment/didi_arrow.png";
 import gift from "@/images/payment/gift.svg";
-import img_share from "@/images/payment/img_share_happy.png";
-import img_top_right from "@/images/payment/content_end/img_top_right.png";
-import img_bottom_left from "@/images/payment/content_end/img_bottom_left.png";
-import img_center from "@/images/payment/content_end/img_center.png";
+import { motion, useInView } from 'motion/react';
+import { useRef } from 'react';
 const ContentEnd = () => {
     const locale = useLocale();
     const payment = locale === 'en' ? enBlog : ziBlog;
@@ -23,18 +24,29 @@ const ContentEnd = () => {
     const receiveCard = payment.shareHappyMoment.receiveCard;
     {/* Content Section: Card */ }
     const cardSection = payment.cardSection;
+
+    const textRef = useRef(null);
+    const isInView = useInView(textRef, { once: false, amount: 0.5 });
+
     return (
         <div className='pt-20 pb-30 container mx-auto'>
-            <div className="pb-30 text-center">
+            <motion.div
+                ref={textRef}
+                initial={{ opacity: 0, y: 20 }}
+                animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
+                transition={{ duration: 0.8, ease: 'easeOut' }}
+                className="pb-30 text-center"
+            >
                 <h1 className="md:text-6xl text-3xl font-semibold pt-4 sm:pt-6 lg:pt-10">
                     {cardSection.title}
                 </h1>
                 <p className="text-gray-500 mt-2 sm:mt-4 mx-auto w-full sm:w-[50%] lg:w-[20%] text-sm sm:text-base">
                     {cardSection.description}
                 </p>
-            </div>
+            </motion.div>
 
-            <div className="pt-30 flex flex-col md:flex-row items-center justify-between">
+
+            <div className="pt-30 flex flex-col md:flex-row items-center justify-center">
                 <div className="flex flex-col w-full md:w-[35%]">
                     <Image src={gift} alt="Share Happy Moment" />
 
@@ -46,7 +58,7 @@ const ContentEnd = () => {
                     </p>
 
                     <div className='pt-22'>
-                        <div className="flex items-center justify-between bg-orange-500 text-white rounded-2xl p-2 sm:p-3 md:p-4 shadow-md md:w-[400px]">
+                        <div className="flex items-center justify-between bg-orange-500 text-white rounded-2xl p-2 sm:p-3 md:p-4 shadow-md md:w-[400px] animate-translateX">
                             <div className="flex items-center gap-3">
                                 <div className="flex-shrink-0">
                                     <Image src={icon_gift} alt="Gift Icon" width={24} height={24} className="w-8 h-8 md:w-12 md:h-12" />
@@ -60,7 +72,7 @@ const ContentEnd = () => {
                                 <p className="text-base sm:text-lg font-semibold">{giftCard.amount}</p>
                             </div>
                         </div>
-                        <div className="flex items-center justify-between bg-green-400 text-white rounded-2xl p-2 sm:p-3 md:p-4 shadow-md mt-6 ml-0 md:ml-20 md:w-[400px]">
+                        <div className="flex items-center justify-between bg-green-400 text-white rounded-2xl p-2 sm:p-3 md:p-4 shadow-md mt-6 ml-0 md:ml-20 md:w-[400px] animate-translate-rightX">
                             <div className="flex items-center gap-3">
                                 <div className="flex-shrink-0">
                                     <Image src={icon_arrow} alt="Receive Icon" width={24} height={24} className="w-8 md:w-12 h-8 md:h-12" />
