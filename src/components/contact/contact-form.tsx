b0ea970569aeb79dcb0610e0a678f2b149d63ea3
tin.nd <tinn948@gmail.com>
2025-06-19 22:59:02 +0700

    fix: translate contact us + fix reneder

--- src/components/contact/contact-form.tsx
+++ src/components/contact/contact-form.tsx
@@ -9,94 +9,105 @@ import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import { useForm } from 'react-hook-form';
 import { ContactUsForm } from '@/interfaces/contact-form';
+import { useTranslations } from 'next-intl';
+import SelectInput from './select-input';
 
 
 
 
-const schema = yup
-    .object({
-        firstName: yup.string().required('First name is required'),
-        lastName: yup.string().required('Last name is required'),
-        email: yup.string().required('Email is required'),
-        companyName: yup.string().required('Company name is required'),
-        companyWebsite: yup.string().required('Company website is required'),
-        message: yup.string().required('Message is required'),
-        phone: yup.string().required('Phone is required'),
-    })
-    .required()
+
 
 
 const ContactForm = () => {
 
     const [enabled, setEnabled] = useState(false);
+    const t = useTranslations("contactUs");
+
+    const schema = yup
+        .object({
+            firstName: yup.string().required(`${t("firstName")} ${t("isRequire")}`),
+            lastName: yup.string().required(`${t("lastName")} ${t("isRequire")}`),
+            email: yup.string().required(`${t("email")} ${t("isRequire")}`),
+            companyName: yup.string().required(`${t("companyName")} ${t("isRequire")}`),
+            companyWebsite: yup.string().required(`${t("companyWebsite")} ${t("isRequire")}`),
+            message: yup.string().required(`${t("message")} ${t("isRequire")}`),
+            typeCollaboration: yup.string().required(`${t("typeCollaboration")} ${t("isRequire")}`),
+            phone: yup.string().required(`${t("phone")} ${t("isRequire")}`),
+        })
+        .required()
 
     const {
         register,
         handleSubmit,
         formState: { errors },
+        setValue
     } = useForm({
         resolver: yupResolver(schema),
     })
 
-    console.log('errors', errors)
     const onSubmit = (data: ContactUsForm, event: any) => {
         event.preventDefault()
     }
 
     return (
         <div className='grow relative'>
-            <div className="p-8">
-                <h2 className="text-[#1b1b1b] text-[48px] font-semibold -tracking-[1.64px] mb-2">Contact Us</h2>
+            <div>
+                <h2 className="text-[#1b1b1b] text-[48px] font-semibold -tracking-[1.64px] mb-2">{t("title")}</h2>
                 <p className="text-sm text-[#666] font-normal mb-6">
-                    We handle all law enforcement requests from any jurisdiction in strict compliance with our Terms & Conditions, Privacy Policy, and all relevant laws and regulations.
+                    {t("desc")}
                 </p>
 
                 <form className="space-y-4" onSubmit={handleSubmit(onSubmit)} >
                     <div className='grid grid-cols-2 gap-4'>
                         <div>
-                            <NameInput inputType='text' name="firstName" label='First Name' type='name' placeholder='First Name' register={register} />
+                            <NameInput inputType='text' name="firstName" label={t("firstName")} type='name' placeholder={t("firstName")} register={register} />
                             <p className='text-sm text-red-700 mt-1'>{errors.firstName?.message}</p>
                         </div>
                         <div>
-                            <NameInput inputType='text' name='lastName' label='Last Name' type='name' placeholder='Last Name' register={register} />
+                            <NameInput inputType='text' name='lastName' label={t("lastName")} type='name' placeholder={t("lastName")} register={register} />
                             <p className='text-sm text-red-700 mt-1'>{errors.lastName?.message}</p>
                         </div>
                     </div>
 
                     <div className='grid grid-cols-2 gap-4'>
                         <div>
-                            <NameInput name='email' inputType='email' placeholder='Your Work Email' register={register} />
+                            <NameInput name='email' inputType='email' placeholder={t('email')} register={register} />
                             <p className='text-sm text-red-700 mt-1'>{errors.email?.message}</p>
                         </div>
                         <div>
-                            <NumberInput name="phone" placeholder="Phone Number" register={register} />
+                            <NumberInput name="phone" placeholder={t("phone")} register={register} />
                             <p className='text-sm text-red-700 mt-1'>{errors.phone?.message}</p>
                         </div>
                     </div>
 
                     <div className='grid grid-cols-2 gap-4'>
                         <div>
-                            <NameInput inputType='text' name='companyName' placeholder='Company Name' register={register} />
+                            <NameInput inputType='text' name='companyName' placeholder={t("companyName")} register={register} />
                             <p className='text-sm text-red-700 mt-1'>{errors.companyName?.message}</p>
                         </div>
                         <div>
-                            <NameInput inputType='text' name='companyWebsite' placeholder='Company Website' register={register} />
+                            <NameInput inputType='text' name='companyWebsite' placeholder={t("companyWebsite")} register={register} />
                             <p className='text-sm text-red-700 mt-1'>{errors.companyWebsite?.message}</p>
                         </div>
                     </div>
 
                     <div>
-                        <textarea {...register('message')} id="" placeholder='Message' className={`w-full p-4 rounded-[10px] bg-transparent border border-[rgba(216,216,216,0.64)] outline-none text-base font-medium text-[#0E121B] placeholder:text-[#aeaeae] placeholder:font-normal`} />
+                        <SelectInput setValue={setValue} />
+                        <p className='text-sm text-red-700 mt-1'>{errors.typeCollaboration?.message}</p>
+                    </div>
+
+                    <div>
+                        <textarea {...register('message')} id="" placeholder={t("message")} className={`w-full p-4 rounded-[10px] bg-transparent border border-[rgba(216,216,216,0.64)] outline-none text-base font-medium text-[#0E121B] placeholder:text-[#aeaeae] placeholder:font-normal`} />
                         <p className='text-sm text-red-700 mt-1'>{errors.message?.message}</p>
                     </div>
 
                     <div className='flex items-center gap-4'>
                         <CheckboxInput enabled={enabled} setEnabled={setEnabled} />
-                        <p className='w-[384px] text-[#666] text-sm font-medium'>If you do not wish to receive marketing and promotional materials from NewPay, please check the box.</p>
+                        <p className='w-[340px] text-[#666] text-sm font-medium'>{t("checkBox")}</p>
                     </div>
 
                     <button disabled={!enabled} className='absolute bottom-10 right-[-100px] z-[10] h-20 box-border flex w-[267px] cursor-pointer'>
-                        <span style={{ borderRadius: '16px 0px 0px 16px' }} className='py-4 px-6 h-full box-border flex items-center bg-[#FF6910] text-white font-medium text-2xl'>Submit Now</span>
+                        <span style={{ borderRadius: '16px 0px 0px 16px' }} className='py-4 px-6 h-full box-border flex items-center bg-[#FF6910] text-white font-medium text-2xl'>{t("submit")}</span>
                         <p style={{ borderRadius: '0px 16px 16px 0px' }} className='bg-white py-4 px-6 flex items-center'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
                                 <path d="M9.33203 22.6668L22.6654 9.3335" stroke="#1B1B1B" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
---
+++ src/components/contact/select-input.tsx
@@ -0,0 +1,74 @@
+"use client"
+import { ContactUsForm } from '@/interfaces/contact-form'
+import React, { useState } from 'react'
+import { UseFormSetValue } from 'react-hook-form'
+
+
+const CollaborationTypes = [
+    {
+        value: "General Inquiries",
+        id: 1
+    },
+    {
+        value: "Affiliate Program",
+        id: 2
+    },
+    {
+        value: "Partnerships",
+        id: 3
+    },
+    {
+        value: "Careers",
+        id: 4
+    },
+]
+
+interface SelectInputProps {
+    setValue: UseFormSetValue<ContactUsForm>
+}
+
+
+const SelectInput = ({ setValue }: SelectInputProps) => {
+
+    const [open, setOpen] = useState(false);
+    const [typeSelected, setTypeSelected] = useState("");
+
+
+    const handleOpen = () => {
+        setOpen(!open);
+    }
+
+
+    const handleSetTypeSelected = (value: string) => {
+        setTypeSelected(value);
+        setValue('typeCollaboration', value);
+        handleOpen();
+    }
+
+    return (
+        <div className='relative'>
+            <div onClick={handleOpen} className='border-[rgba(216,216,216,0.64)] cursor-pointer h-[56px] flex justify-between gap-4 relative bg-white rounded-[10px] border p-4 transition-colors outline-none'>
+                <p className={`${typeSelected ? "text-[#0E121B]" : "text-[#aeaeae]"} text-base font-normal`}>{typeSelected ? typeSelected : "Collaboration Type"}</p>
+                <figure>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                        <path d="M5 7.5L10 12.5L15 7.5" stroke="#AEAEAE" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </figure>
+            </div>
+
+            {open && <>
+
+                <div style={{ boxShadow: "0px 12px 40px -2px rgba(0, 0, 0, 0.12)" }} className='absolute w-full h-[170px] left-0 top-[66px] z-[20] p-1 bg-white border border-[rgba(216,216,216,0.64)] rounded-2xl'>
+                    {CollaborationTypes.map((item, index) => {
+                        return <div key={index} onClick={() => handleSetTypeSelected(item.value)} className='p-2 cursor-pointer hover:bg-black/4 rounded'>
+                            {item.value}
+                        </div>
+                    })}
+                </div>
+
+            </>}
+        </div>
+    )
+}
+
+export default SelectInput
