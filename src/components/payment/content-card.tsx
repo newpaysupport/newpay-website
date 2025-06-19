e73c7c1dd4450a61cf087bbe998c5b4093917625
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-20 00:40:13 +0700

    fix : image card and svg

--- src/components/payment/content-card.tsx
+++ src/components/payment/content-card.tsx
@@ -4,8 +4,10 @@ import { payment as ziBlog } from '@/i18n/messages/zi.json';
 import { useLocale } from 'next-intl';
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
-import card_payment from "@/images/payment/Front.png";
-import wallet from "@/images/payment/WALLET.png";
+// import card_payment from "@/images/payment/Front.png";
+import wallet_no_card from "@/images/payment/content-card/wallet_no_card.svg";
+import card_payment from "@/images/payment/content-card/card_payment.svg";
+import wallet_has_card from "@/images/payment/content-card/Wallet.svg";
 
 const ContentCard = () => {
     const locale = useLocale();
@@ -50,13 +52,13 @@ const ContentCard = () => {
                         src={card_payment}
                         alt="card payment"
                         ref={cardRef}
-                        className={`md:w-[390px] md:h-[245px] mx-auto transition-transform duration-300 ease-linear ${isSticky ? 'sticky top-[30%]' : ''} ${isCardVisible ? 'opacity-100' : 'opacity-0'}`}
+                        className={`mx-auto transition-transform duration-300 ease-linear ${isSticky ? 'sticky top-[30%]' : ''} ${isCardVisible ? 'opacity-100' : 'opacity-0'}`}
                     />
                     <div className='pt-20'>
                         {
                             isCardVisible ? 
-                            <Image src={wallet} alt="Wallet no card" ref={walletRef} className={`mx-auto w-[430px] h-[320px]`} /> 
-                            : <Image src={card_payment} alt="Wallet has card" ref={walletRef} className='mx-auto w-[430px] h-[320px]' />
+                            <Image src={wallet_no_card} alt="Wallet no card" ref={walletRef} className={`mx-auto w-[430px] h-[320px]`} /> 
+                            : <Image src={wallet_has_card} alt="Wallet has card" ref={walletRef} className='mx-auto w-[430px] h-[320px]' />
                         }
                     </div>
                 </div>
