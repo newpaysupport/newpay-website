8113f0463df161a6fd1d8e49b8599103108bc857
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-20 23:16:53 +0700

    fix : text-color and width and height

--- src/components/payment/index.tsx
+++ src/components/payment/index.tsx
@@ -56,9 +56,9 @@ const PaymentScreen = () => {
             {/* Hero Section */}
             <div className="relative">
                 <Image src={hero_payment} alt="background" width={0} height={0} className="object-cover md:w-full md:h-full h-[1000px]" />
-                <p className="fadeInUp-no-delay md:text-sm md:w-[20%] w-[50%] text-xs text-gray-600 text-right absolute top-[30%] bottom-[60%] right-[12%]">{heroSection.description}</p>
+                <p className="fadeInUp-no-delay font-normal md:w-[20%] w-[50%] text-sm text-[#AEAEAE] text-right absolute top-[30%] bottom-[60%] right-[12%]">{heroSection.description}</p>
                 <div className="absolute md:right-[9.2%] bottom-[24%]">
-                    <Image src={card_container} alt="background" width={0} height={0} className='fadeInUp-no-delay' />
+                    <Image src={card_container} alt="background" width={0} height={0} className='fadeInUp-no-delay w-[670px] h-[300px]' />
                 </div>
                 <div className="absolute md:w-[25%] w-[50%] top-[30%] bottom-[60%] left-[12%]">
                     <Image src={logo_payment} alt="background" width={0} height={0} className="fadeInUp-no-delay w-12 h-12 mb-10" />
@@ -67,10 +67,10 @@ const PaymentScreen = () => {
                         backgroundClip: 'text',
                         WebkitBackgroundClip: 'text',
                         WebkitTextFillColor: "transparent"
-                    }} className="fadeInUp-no-delay md:text-5xl md:my-6 md:leading-14 my-4 text-white leading-8">
+                    }} className="fadeInUp-no-delay md:text-5xl md:my-6 md:leading-14 my-4 text-white leading-8 font-medium">
                         {heroSection.title}
                     </h1>
-                    <button className="fadeInUp-no-delay rounded-full md:py-4 md:px-8 text-md font-semibold p-2 text-white cursor-pointer bg-orange-500 hover:bg-orange-600">{heroSection.buttonText}</button>
+                    <button className="fadeInUp-no-delay rounded-full md:py-4 md:px-8 text-md font-semibold p-2 text-[#FFF] cursor-pointer bg-orange-500 hover:bg-orange-600">{heroSection.buttonText}</button>
                 </div>
             </div>
 
