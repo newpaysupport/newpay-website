7615df36c489b729759a4aa6bb13518656b17f76
tin.nd <tinn948@gmail.com>
2025-06-21 23:25:13 +0700

    fix: check accesibility vital footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -88,9 +88,11 @@ const Footer = () => {
                                 <h4 className='text-white text-sm font-semibold mb-6'>{menu.title}</h4>
                                 <ul>
                                     {menu.items.map((item, index) => {
-                                        return <Link href={`/${currentLocale}${item.link}`} key={index} className='mb-2 block'>
-                                            <li className='text-[#848484] text-sm font-semibold'>{item.label}</li>
-                                        </Link>
+                                        return <li key={index} className='mb-2'>
+                                            <Link href={`/${currentLocale}${item.link}`} className='text-[#848484] text-sm font-semibold block'>
+                                                {item.label}
+                                            </Link>
+                                        </li>
                                     })}
                                 </ul>
                             </div>
