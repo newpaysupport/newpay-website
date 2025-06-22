90cef92774d68659c72189a0608037428416b4c7
tin.nd <tinn948@gmail.com>
2025-06-22 14:54:23 +0700

    fix: bugs

--- src/components/contact/name-input.tsx
+++ src/components/contact/name-input.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { ContactUsForm } from '@/interfaces/contact-form';
 import React, { useState } from 'react'
-import { UseFormRegister } from 'react-hook-form';
+import { Control, UseFormRegister, useWatch } from 'react-hook-form';
 
 
 interface NameInputProps {
@@ -15,7 +15,7 @@ interface NameInputProps {
 }
 
 
-const NameInput = ({ label, name, type = 'normal', placeholder, register, inputType }: NameInputProps) => {
+const NameInput = ({ label, name, type = 'normal', placeholder, register, inputType, control }: NameInputProps & { control: Control<ContactUsForm> }) => {
 
     const [focused, setFocused] = useState<string | null>(null);
 
@@ -24,23 +24,34 @@ const NameInput = ({ label, name, type = 'normal', placeholder, register, inputT
         setFocused(name)
     }
 
+    const fieldValue = useWatch({
+        control,
+        name,
+    });
+
+    const hasValue = fieldValue && fieldValue.trim() !== '';
+
+    const isAbove = (focused === name || hasValue)
+
+    const classAbove = isAbove ? "left-4 top-[-22%] bg-white" : " top-[16px]";
+    const classSpanOnFocusInput = focused === name ? "text-[#FF6910]" : "text-[#aeaeae]";
+    const classContainerOnFocusInput = focused === name ? "border-[#FF6910] border-[1.5px]" : "border-[rgba(216,216,216,0.64)]";
+
     return (
-        <div className={`${focused === name ? "border-[#FF6910] border-[1.5px]" : "border-[rgba(216,216,216,0.64)]"} h-[56px] flex-1 relative bg-white rounded-[10px] border p-4 transition-colors outline-none`}>
-            {type === 'name' && <span className={`${focused === name ? "text-[#FF6910] left-4 top-[-22%] bg-white" : "text-[#aeaeae]"} text-sm absolute capitalize font-medium pointer-events-none`}
+        <div className={`${classContainerOnFocusInput} h-[56px] flex-1 relative bg-white rounded-[10px] border p-4 transition-colors outline-none`}>
+            <span className={`${classAbove} ${classSpanOnFocusInput} text-sm absolute capitalize font-medium pointer-events-none`}
                 style={{
                     transition: 'all 0.3s cubic-bezier(0.4, 0, 0.2, 1)'
                 }}>
                 {label}
-            </span>}
+            </span>
             <input
                 type={inputType}
-                // value={first}
-                // onChange={(e) => handleFirstChange(e.target.value)}
                 {...register(name)}
                 onFocus={() => handleFocus()}
                 onBlur={() => setFocused(null)}
                 placeholder={placeholder}
-                className={`w-full bg-transparent border-0 outline-none text-sm font-medium text-[#0E121B] placeholder:text-[#aeaeae] placeholder:font-normal`}
+                className={`w-full bg-transparent border-0 outline-none text-sm font-medium text-[#0E121B] bg-none placeholder:text-[#aeaeae] placeholder:font-normal`}
             />
         </div>
     )
