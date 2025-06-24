1e7061e9e16451e3b6fe5c975377e1008a009085
tin.nd <tin.nd@vgamestd.com>
2025-06-24 17:47:32 +0700

    fix: responsive page download

--- src/components/download/banner.tsx
+++ src/components/download/banner.tsx
@@ -1,14 +1,13 @@
 "use client"
-import React from 'react'
-import newpayMask from '@/images/download/newpay_square_mark.png'
+import appStore1 from '@/images/download/app-store 1.png'
 import appStore from '@/images/download/apple-app-store.svg.svg'
+import googleStore1 from '@/images/download/google-play-store 1.png'
 import googleStore from '@/images/download/google-play-store.svg.svg'
+import newpayMask from '@/images/download/newpay_square_mark.png'
 import product from '@/images/download/product.png'
-
-
-import Image from 'next/image'
-import Link from 'next/link'
+import productMobile from '@/images/download/product-mobile.png'
 import { useTranslations } from 'next-intl'
+import Image from 'next/image'
 import toast from 'react-hot-toast'
 import ToastCustom from '../common/toast'
 
@@ -23,12 +22,12 @@ const Banner = () => {
 
     return (
         <div className='bg-[#060606] pt-[120px]'>
-            <div className='container mx-auto lg:px-[120px]'>
+            <div className='container mx-auto lg:px-[120px] px-4'>
                 <div>
                     <p className='text-[#FF6910] text-xl font-semibold text-center opacity-0 fadeInUp-no-delay'>{t("heading")}</p>
-                    <div className='textTitleClip text-[72px] text-center font-semibold' >
+                    <div className='textTitleClip text-[40px] lg:text-[72px] text-center font-semibold' >
                         <span>{t("title1")}</span> <br />
-                        <div className='flex justify-center items-center gap-x-2'>
+                        <div className='flex justify-center flex-wrap items-center gap-x-2'>
                             <span>{t("title2")}</span>
                             <figure className='min-w-[72px] w-[72px] h-[72px]'>
                                 <Image src={newpayMask} alt='' />
@@ -36,27 +35,45 @@ const Banner = () => {
                             <span>{t("title3")}</span>
                         </div>
                     </div>
-                    <p className='my-10 text-[#aeaeae] text-lg font-medium text-center opacity-0 fadeInUp-delay-03'>
+                    <p className='my-10 text-[#aeaeae] text-sm lg:text-lg font-medium text-center opacity-0 fadeInUp-delay-03'>
                         {t("desc1")} <br />
                         {t("desc2")}
                     </p>
-                    <div className='flex items-center justify-center gap-x-6 opacity-0 fadeInUp-delay-06'>
+                    <div className='flex items-center justify-center gap-x-4 lg:gap-x-6 opacity-0 fadeInUp-delay-06'>
+
+
                         <button
                             onClick={handleToast}
-                            className='hover:bg-[#212121] btnCta transition-all ease-linear duration-300 w-[287px] h-[84px] py-[10px] rounded-2xl flex items-center justify-center cursor-pointer'>
-                            <Image src={appStore} alt='' />
+                            className='block lg:hidden cursor-pointer'>
+                            <Image src={appStore1} alt='' className='object-contain' />
+
                         </button>
 
                         <button
                             onClick={handleToast}
-                            className='hover:bg-[#212121] btnCta transition-all ease-linear duration-300 w-[287px] h-[84px] py-[10px] rounded-2xl flex items-center justify-center cursor-pointer'>
-                            <Image src={googleStore} alt='' />
+                            className='block lg:hidden cursor-pointer'>
+                            <Image src={googleStore1} alt='' className='object-contain' />
+                        </button>
+
+                        {/* desktop */}
+                        <button
+                            onClick={handleToast}
+                            className='hover:bg-[#212121] btnCta transition-all ease-linear duration-300 lg:w-[287px] h-[84px] py-[10px] rounded-2xl hidden lg:flex items-center justify-center cursor-pointer'>
+                            <Image src={appStore} alt='' className='object-contain' />
+
+                        </button>
+
+                        <button
+                            onClick={handleToast}
+                            className='hover:bg-[#212121] btnCta transition-all ease-linear duration-300 lg:w-[287px] h-[84px] py-[10px] rounded-2xl hidden lg:flex items-center justify-center cursor-pointer'>
+                            <Image src={googleStore} alt='' className='' />
                         </button>
                     </div>
                 </div>
 
-                <figure className='flex justify-center mt-[40px]'>
-                    <Image src={product} alt='' priority />
+                <figure className='flex mt-[40px]'>
+                    <Image src={product} alt='' priority className='hidden lg:block' />
+                    <Image src={productMobile} alt='' priority className='lg:hidden block w-full' />
                 </figure>
             </div>
         </div>
