e4208c61c5492922f2bad9cbdcbd789c17a31080
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-25 01:37:49 +0700

    fix : responsive download section

--- src/components/payment/download-section.tsx
+++ src/components/payment/download-section.tsx
@@ -14,12 +14,23 @@ const DownloadSection = () => {
         <section className="bg-[#060606]">
             <div className="bg-[#FFFFFF]/4 md:py-20 md:px-40 px-6 py-12 md:rounded-[80px] rounded-[24px]">
                 <div className="flex md:flex-row flex-col-reverse max-w-7xl mx-auto md:gap-30 md:justify-center items-center">
-                    <Image src={product_content_end} alt="Gift Icon" width={540} height={540} className="w-full h-full md:w-[540px] md:h-[540px]"/>
+                    <Image src={product_content_end} alt="Gift Icon" width={540} height={540} className="w-full h-full md:w-[540px] md:h-[540px]" />
                     <div className='md:p-0 p-6 md:w-1/2 md:text-left text-center md:pb-0 pb-8'>
                         <Image src={logo_payment} alt="Gift Icon" width={100} height={100} className="w-16 h-16 md:w-20 md:h-20 md:m-0 mx-auto" />
-                        <h1 className="md:text-5xl md:leading-14 text-2xl font-semibold pt-4 lg:pt-10 text-left text-white">
-                            {t('title')}
+                        <h1 className="md:text-5xl md:leading-14 md:text-left text-center text-2xl font-semibold pt-4 lg:pt-10  text-white">
+                            {locale === 'en' ? (
+                                <>
+                                    Join NewPay.<br />
+                                    Spend Safe & Smart
+                                </>
+                            ) : (
+                                <>
+                                    加入 NewPay.<br />
+                                    开启安全精明的消费方式
+                                </>
+                            )}
                         </h1>
+
                         <Link href={`/${locale}/download`}>
                             <button
                                 className="md:text-lg rounded-2xl text-sm md:mt-10 mt-4 px-4 py-3 md:p-4 text-black cursor-pointer bg-white hover:bg-gray-100 font-semibold">
