00f2d206dd9cfce8e25a6c91e640164417c57128
tin.nd <tin.nd@vgamestd.com>
2025-06-24 18:13:24 +0700

    feat: responsive blog - partial

--- src/components/blogs/index.tsx
+++ src/components/blogs/index.tsx
@@ -13,10 +13,10 @@ const BlogScreen = () => {
     return (
         <div className='pt-[128px] container mx-auto'>
             {/* Header Section */}
-            <div className='flex flex-col justify-center items-center'>
+            <div className='flex flex-col justify-center items-center px-4 lg:px-0'>
                 <Image src={blog_logo} alt="Blog Logo" className='w-20 h-20' />
-                <h1 className='text-[60px] font-semibold text-center'><span className='text-[#FF6910]'>NewPay</span> {t('title')}</h1>
-                <p className='text-center text-[#666666] mt-5 text-md font-medium'>{t('subtitle')}</p>
+                <h1 className='text-[32px] lg:text-[60px] font-semibold text-center'><span className='text-[#FF6910]'>NewPay</span> {t('title')}</h1>
+                <p className='text-center text-[#666666] mt-5 text-sm font-medium'>{t('subtitle')}</p>
                 <div className="mt-10 flex items-center h-[56px] w-[320px] bg-[#f8f8f8] border border-[#d8d8d8] rounded-full ">
                     <button type="button" className="h-full px-3">
                         <Image src={search} alt="Search Icon" width={20} height={20} />
@@ -30,9 +30,9 @@ const BlogScreen = () => {
                 tags={t.raw('tags')}
                 blogTag={t('title')}
             />
-            <div className='w-full flex justify-center py-20'>
+            {/* <div className='w-full flex justify-center py-20'>
                 <button className='cursor-pointer rounded-full px-12 py-5 border border-[#D8D8D8] text-lg font-semibold text-[#1B1B1B]'>Load more</button>
-            </div>
+            </div> */}
         </div>
     )
 }
