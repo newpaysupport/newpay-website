7e615f3a8939360a6c719404c12b8037bf771026
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-26 19:44:52 +0700

    fix  : responsive goble-listcard

--- src/components/home/gift-card/globe-listcard.tsx
+++ src/components/home/gift-card/globe-listcard.tsx
@@ -4,7 +4,6 @@ import { home as en } from '@/i18n/messages/en.json';
 import { home as zi } from '@/i18n/messages/zi.json';
 import globe from '@/images/home/giftcard/Globe.png';
 import shadow_globe from '@/images/home/giftcard/shadow_globe.png';
-import card4_mobile from '@/images/home/giftcard/card.png'
 import { useLocale } from 'next-intl';
 import Image from 'next/image';
 
@@ -19,54 +18,51 @@ const GlobeListCard = () => {
         { id: 3, image: globeListCard[2].image },
         { id: 4, image: globeListCard[1].image },
     ]
+
     return (
         <div className='bg-[#060606] relative'>
             <div className='pt-60'>
                 <Image src={globe} alt="Globe Image" className="w-full md:h-full h-[600px] object-cover" />
 
                 {/* Desktop */}
-                <div className='absolute right-0 left-0 top-[20%] bottom-0 md:flex gap-5 items-start justify-center hidden'>
+                <div className='absolute right-0 left-0 top-[20%] bottom-0 md:flex gap-5 items-start justify-center hidden h-fit z-10'>
                     {globeListCard.map((item, index) => {
                         let rotation = 0;
                         if (index === 0) rotation = -8;
                         else if (index === 3) rotation = 8;
                         return (
                             <div key={item.title} className='relative overflow-hidden group hover:-translate-y-5 transition-all ease-linear duration-500 cursor-pointer'>
-                                <div>
+                                <div className='relative'>
                                     <Image src={item.image} width={280} height={280} alt="Globe Image" className="object-cover" />
-                                </div>
-                                <div style={{
-                                    transform: `skewy(${rotation}deg)`,
-
-
-                                }}
-                                    className={`absolute right-[18px] left-[20px]
-                                    ${index === 3 || index === 0 ? 'bottom-12' : 'bottom-10'}
-                                     transition-all duration-500 ease-in-out`}>
-                                    <h2 className={`
-                                    text-white text-xl font-medium my-2
-                                    ${index === 3 || index === 0 ? '' : 'transform translate-y-10'} transform translate-y-5 transition-all duration-500 ease-in-out
-                                    group-hover:translate-y-0
-                                    `}>
-                                        {item.title}
-                                    </h2>
-                                    <p
-                                        className='text-[#848484] text-sm
-                                    transform translate-y-10 opacity-0
-                                    transition-all duration-500 ease-in-out
-                                    group-hover:translate-y-0 group-hover:opacity-100'
-                                    >{item.desc}
-                                    </p>
+                                    <div style={{
+                                        transform: `skewY(${rotation}deg)`,
+                                    }}
+                                        className={`absolute right-[18px] left-[20px]
+                                        ${index === 3 || index === 0 ? 'bottom-12' : 'bottom-10'}
+                                         transition-all duration-500 ease-in-out`}>
+                                        <h2 className={`
+                                        text-white text-xl font-medium my-2
+                                        ${index === 3 || index === 0 ? 'transform translate-y-5' : 'transform translate-y-10'} 
+                                        transition-all duration-500 ease-in-out
+                                        group-hover:translate-y-0
+                                        `}>
+                                            {item.title}
+                                        </h2>
+                                        <p className='text-[#848484] text-sm
+                                        transform translate-y-10 opacity-0
+                                        transition-all duration-500 ease-in-out
+                                        group-hover:translate-y-0 group-hover:opacity-100'>
+                                            {item.desc}
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         )
                     })}
                 </div>
 
-
-
                 {/* Mobile */}
-                {/* <div className="absolute right-0 left-0 bottom-0 top-[20%] md:hidden">
+                <div className="absolute right-0 left-0 bottom-0 top-[20%] md:hidden">
                     <div className="flex gap-5 items-start justify-start overflow-x-auto flex-nowrap px-4">
                         {listImg.map((img, index) => {
                             const item = globeListCard[index];
@@ -98,16 +94,12 @@ const GlobeListCard = () => {
                             );
                         })}
                     </div>
-                </div> */}
+                </div>
 
-
-                <div className='absolute inset-0 bottom-0 left-0 md:flex items-end justify-center hidden'>
-                    <div>
-                        <Image src={shadow_globe} alt="Globe Image" />
-                    </div>
+                <div className='absolute inset-0 bottom-0 left-0 md:flex items-end justify-center hidden z-0'>
+                    <Image src={shadow_globe} alt="Globe Image" />
                 </div>
             </div>
-
         </div>
     )
 }
