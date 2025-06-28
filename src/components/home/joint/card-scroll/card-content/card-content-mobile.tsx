d160c4b9f9e0f64a723dbf7692f59904ace0595c
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-06-28 23:41:11 +0700

    fix : card-scroll

--- src/components/home/joint/card-scroll/card-content/card-content-mobile.tsx
+++ src/components/home/joint/card-scroll/card-content/card-content-mobile.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import card2_img2 from '@/images/home/joint/card2_img2.png'
-import card1_img1 from '@/images/home/joint/card1_img1.png'
-import { FaArrowRight } from "react-icons/fa";
-import arrowright from '@/images/home/joint/arrowright.svg'
-import Image from "next/image"
+'use client'
+import arrowright from '@/images/home/joint/arrowright.svg';
+import card1_img1 from '@/images/home/joint/card1_img1.png';
+import card2_img2 from '@/images/home/joint/card2_img2.png';
+import Image from "next/image";
 import Link from 'next/link';
+import { FaArrowRight } from "react-icons/fa";
 
 
 const CardContentMobile = ({ joint, locale }: { joint: any; locale: string }) => {
