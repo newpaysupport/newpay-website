be6176322f42cb03f9ff3ba3bac43a32d751389d
tin.nd <tinn948@gmail.com>
2025-06-30 13:27:58 +0700

    fix: faq translate

--- src/components/home/faq/index.tsx
+++ src/components/home/faq/index.tsx
@@ -1,16 +1,6 @@
-import Faq from '@/components/payment/faq'
-import { useLocale } from 'next-intl';
-import React from 'react'
-import { payment as enBlog } from '@/i18n/messages/en.json';
-import { payment as ziBlog } from '@/i18n/messages/zi.json';
+import Faq from '@/components/payment/faq';
 
 const FAQ = () => {
-    const locale = useLocale();
-    const payment = locale === 'en' ? enBlog : ziBlog;
-
-    {/* Content Section: FAQ */ }
-    const faqSection = payment.faqSection;
-    const faqItems = faqSection.faqItems;
 
     return (
         <div className='bg-[#060606]'>
