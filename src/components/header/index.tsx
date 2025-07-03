1e5823825721890066d22decb886d4dc090613ef
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-07-03 19:22:09 +0700

    fix : header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -175,7 +175,7 @@ const Header = () => {
 
     return (
         <nav style={getHeaderStyles()}
-            className={`text-sm h-[70px] flex items-center justify-between px-6 lg:px-16 2xl:px-32 py-4
+            className={`text-sm h-[70px] flex items-center justify-between px-6 lg:px-20 py-4
                 fixed top-0 left-0 w-full z-50
                 text-white transition-all duration-300 ease-out
                 ${isVisible ? 'translate-y-0' : 'translate-y-[-100%]'}`}>
