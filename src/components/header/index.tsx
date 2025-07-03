1542b4e2537df0296344d110934d387bdff78d45
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-07-03 19:03:02 +0700

    fix : BoxWarper

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -11,6 +11,7 @@ import SwitchLanguage from '../switch-language';
 import CompanyDropdown from "./company-dropdown";
 import MobileHeader from "./mobile-header";
 import PaymentDropdown from "./payment-dropdown";
+import BoxWrapped from "../common/box-wrapped";
 
 const Header = () => {
     const locale = useLocale();
@@ -173,131 +174,133 @@ const Header = () => {
     const img_logo = isBlogAndContact ? logo_black : logo;
 
     return (
-        <nav style={getHeaderStyles()}
-            className={`text-sm h-[70px] flex items-center justify-between px-6 lg:px-16 2xl:px-32 py-4
+        <BoxWrapped>
+            <nav style={getHeaderStyles()}
+                className={`text-sm h-[70px] flex items-center justify-between px-6 lg:px-16 2xl:px-32 py-4
                 fixed top-0 left-0 w-full z-50
                 text-white transition-all duration-300 ease-out
                 ${isVisible ? 'translate-y-0' : 'translate-y-[-100%]'}`}>
-            <Link href={`/${locale}`} className="lg:flex lg:items-center lg:space-x-4">
-                <Image src={img_logo} alt="NewPay Logo" width={120} height={25} className="object-contain" />
-            </Link>
+                <Link href={`/${locale}`} className="lg:flex lg:items-center lg:space-x-4">
+                    <Image src={img_logo} alt="NewPay Logo" width={120} height={25} className="object-contain" />
+                </Link>
 
-            {/* Desktop Navigation */}
-            <ul className="flex lg:justify-center items-center lg:space-x-3">
-                {navLinks.map((link) => (
-                    <li key={link.href}>
-                        <Link
-                            href={link.href}
-                            className={`lg:px-6 font-semibold lg:font-normal lg:py-2 px-5 py-2 rounded-full transition
+                {/* Desktop Navigation */}
+                <ul className="flex lg:justify-center items-center lg:space-x-3">
+                    {navLinks.map((link) => (
+                        <li key={link.href}>
+                            <Link
+                                href={link.href}
+                                className={`lg:px-6 font-semibold lg:font-normal lg:py-2 px-5 py-2 rounded-full transition
                             ${isActive(link.href)
-                                    ? isBlogAndContact
-                                        ? 'bg-black/10 text-black'
-                                        : 'bg-white/8 text-white'
-                                    : isBlogAndContact
-                                        ? 'hover:bg-black/10 text-black'
-                                        : 'hover:bg-white/8 text-white'
-                                }
+                                        ? isBlogAndContact
+                                            ? 'bg-black/10 text-black'
+                                            : 'bg-white/8 text-white'
+                                        : isBlogAndContact
+                                            ? 'hover:bg-black/10 text-black'
+                                            : 'hover:bg-white/8 text-white'
+                                    }
                             `}
-                        >
-                            {link.label}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+                            >
+                                {link.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
 
-            {/* Desktop Right Navigation */}
-            <ul className="hidden lg:flex items-center gap-2 ">
-                {navLinksRight.map((link) => (
-                    <li
-                        key={link.key}
-                        onMouseEnter={() => {
-                            if (link.hasDropdown) {
-                                setOpenDropdown(link.key as "payment" | "company");
-                                setHoveringDropdown(link.key as "payment" | "company");
-                            }
-                        }}
-                        onMouseLeave={() => {
-                            if (link.hasDropdown) {
-                                setOpenDropdown(null);
-                                setHoveringDropdown(null);
-                            }
-                        }}
-                    >
-                        <Link
-                            href={link.href}
-                            className={`px-6 py-2 rounded-full transition flex items-center gap-1
+                {/* Desktop Right Navigation */}
+                <ul className="hidden lg:flex items-center gap-2 ">
+                    {navLinksRight.map((link) => (
+                        <li
+                            key={link.key}
+                            onMouseEnter={() => {
+                                if (link.hasDropdown) {
+                                    setOpenDropdown(link.key as "payment" | "company");
+                                    setHoveringDropdown(link.key as "payment" | "company");
+                                }
+                            }}
+                            onMouseLeave={() => {
+                                if (link.hasDropdown) {
+                                    setOpenDropdown(null);
+                                    setHoveringDropdown(null);
+                                }
+                            }}
+                        >
+                            <Link
+                                href={link.href}
+                                className={`px-6 py-2 rounded-full transition flex items-center gap-1
                             ${isActive(link.href)
-                                    ? isBlogAndContact
-                                        ? 'bg-black/10 text-black'
-                                        : 'bg-white/8 text-white'
-                                    : isBlogAndContact
-                                        ? 'hover:bg-black/10 text-black'
-                                        : 'hover:bg-white/8 text-white'
-                                }
+                                        ? isBlogAndContact
+                                            ? 'bg-black/10 text-black'
+                                            : 'bg-white/8 text-white'
+                                        : isBlogAndContact
+                                            ? 'hover:bg-black/10 text-black'
+                                            : 'hover:bg-white/8 text-white'
+                                    }
                             `}
-                        >
-                            {link.label}
-                        </Link>
+                            >
+                                {link.label}
+                            </Link>
 
-                        {/* Desktop Dropdown */}
-                        {link.hasDropdown && openDropdown === link.key && (
-                            <div className={`absolute top-[full] left-0 w-[100vw] pt-4 z-50`}>
-                                {link.key === 'payment' ?
-                                    (<PaymentDropdown link={link} locale={locale} isBlogAndContact={isBlogAndContact} />) :
-                                    (
-                                        <CompanyDropdown link={link} locale={locale} isBlogAndContact={isBlogAndContact} isScrolled={isScrolled} />
-                                    )}
-                            </div>
-                        )}
-                    </li>
-                ))}
-            </ul>
+                            {/* Desktop Dropdown */}
+                            {link.hasDropdown && openDropdown === link.key && (
+                                <div className={`absolute top-[full] left-0 w-[100vw] pt-4 z-50`}>
+                                    {link.key === 'payment' ?
+                                        (<PaymentDropdown link={link} locale={locale} isBlogAndContact={isBlogAndContact} />) :
+                                        (
+                                            <CompanyDropdown link={link} locale={locale} isBlogAndContact={isBlogAndContact} isScrolled={isScrolled} />
+                                        )}
+                                </div>
+                            )}
+                        </li>
+                    ))}
+                </ul>
 
-            <div className="hidden lg:block">
-                <SwitchLanguage locale={locale} switchLocale={switchLocale} t={t} isBlogAndContact={isBlogAndContact} isVisible={isVisible} />
-            </div>
+                <div className="hidden lg:block">
+                    <SwitchLanguage locale={locale} switchLocale={switchLocale} t={t} isBlogAndContact={isBlogAndContact} isVisible={isVisible} />
+                </div>
 
-            {/* Mobile Menu Button */}
-            <button
-                aria-label="menu-btn"
-                type="button"
-                className={`cursor-pointer menu-btn flex items-center justify-center w-10 h-10 lg:hidden active:scale-90 transition-all duration-200
+                {/* Mobile Menu Button */}
+                <button
+                    aria-label="menu-btn"
+                    type="button"
+                    className={`cursor-pointer menu-btn flex items-center justify-center w-10 h-10 lg:hidden active:scale-90 transition-all duration-200
                     ${isBlogAndContact
-                        ? 'text-black hover:bg-black/10'
-                        : 'text-white hover:bg-white/10'
-                    }
+                            ? 'text-black hover:bg-black/10'
+                            : 'text-white hover:bg-white/10'
+                        }
                 `}
-                onClick={toggleMobileMenu}>
+                    onClick={toggleMobileMenu}>
 
-                {isMobileMenuOpen ? (
-                    <span className="text-xl font-light">×</span>
-                ) : (
-                    <div className="flex flex-col gap-1">
-                        <div className={`w-5 h-0.5 ${isBlogAndContact ? 'bg-black' : 'bg-white'}`}></div>
-                        <div className={`w-5 h-0.5 ${isBlogAndContact ? 'bg-black' : 'bg-white'}`}></div>
-                        <div className={`w-5 h-0.5 ${isBlogAndContact ? 'bg-black' : 'bg-white'}`}></div>
-                    </div>
+                    {isMobileMenuOpen ? (
+                        <span className="text-xl font-light">×</span>
+                    ) : (
+                        <div className="flex flex-col gap-1">
+                            <div className={`w-5 h-0.5 ${isBlogAndContact ? 'bg-black' : 'bg-white'}`}></div>
+                            <div className={`w-5 h-0.5 ${isBlogAndContact ? 'bg-black' : 'bg-white'}`}></div>
+                            <div className={`w-5 h-0.5 ${isBlogAndContact ? 'bg-black' : 'bg-white'}`}></div>
+                        </div>
+                    )}
+                </button>
+
+                {/* Mobile Menu */}
+                {isMobileMenuOpen && (
+                    <MobileHeader
+                        locale={locale}
+                        navLinksRight={navLinksRight}
+                        isBlogAndContact={isBlogAndContact}
+                        isVisible={isVisible}
+                        isMobileMenuOpen={isMobileMenuOpen}
+                        toggleMobileDropdown={toggleMobileDropdown}
+                        mobileOpenDropdown={mobileOpenDropdown}
+                        mobileCardOpen={mobileCardOpen}
+                        setMobileCardOpen={setMobileCardOpen}
+                        setIsMobileMenuOpen={setIsMobileMenuOpen}
+                        switchLocale={switchLocale}
+                        t={t}
+                    />
                 )}
-            </button>
-
-            {/* Mobile Menu */}
-            {isMobileMenuOpen && (
-                <MobileHeader
-                    locale={locale}
-                    navLinksRight={navLinksRight}
-                    isBlogAndContact={isBlogAndContact}
-                    isVisible={isVisible}
-                    isMobileMenuOpen={isMobileMenuOpen}
-                    toggleMobileDropdown={toggleMobileDropdown}
-                    mobileOpenDropdown={mobileOpenDropdown}
-                    mobileCardOpen={mobileCardOpen}
-                    setMobileCardOpen={setMobileCardOpen}
-                    setIsMobileMenuOpen={setIsMobileMenuOpen}
-                    switchLocale={switchLocale}
-                    t={t}
-                />
-            )}
-        </nav>
+            </nav>
+        </BoxWrapped>
     );
 };
 export default Header;
--- src/components/payment/faq.tsx
+++ src/components/payment/faq.tsx
@@ -21,48 +21,46 @@ const Faq = () => {
     // const faqSection = payment.faqSection;
     // const faqItems = faqSection.faqItems;
     return (
-        <div className="lg:py-20 lg:px-30 px-6 py-12">
-            <BoxWrapped className="flex lg:flex-row flex-col justify-between mx-auto">
-                <div className="text-white flex lg:flex-col flex-row justify-between lg:h-[450px]">
-                    <div>
-                        <h1 className="lg:text-6xl text-[32px] font-semibold">
-                            {t('title')}
-                        </h1>
-                        <p className="lg:text-base text-sm mt-3 text-[#AEAEAE]">
-                            {t('subtitle')}
-                        </p>
-                        <Link href={`/${locale}/faq-help`} className='lg:block hidden'>
-                            <button className='lg:mt-10 bg-[#1C1C1C] text-lg rounded-2xl px-6 py-4 font-semibold cursor-pointer'>
-                                <p className="flex justify-center items-center gap-3"> {locale === 'en' ? 'View more' : '查看更多'} <Image src={arrow_right} alt='arrow' className="font-light group-hover:translate-x-1.5 transition ease-in-out animate-translateX" /></p>
-                            </button>
-                        </Link>
-                    </div>
-                    <Image src={message} alt="Message Icon" width={205} height={180} className='lg:w-full lg:h-[180px] w-[190px] h-[150px] object-cover object-bottom' />
-                </div>
-                <div className="text-white">
-                    {t.raw("faqItems").map((item: FAQ, index: number) => {
-                        return (
-                            <details key={index} className="cursor-pointer focus:outline-none group border-b border-gray-800 ">
-                                <summary className="lg:w-[640px] w-full flex justify-between items-center text-left py-6">
-                                    <p className="lg:text-2xl text-lg font-medium lg:w-full w-[90%]">{item.question}</p>
-                                    <span className="text-3xl transition-transform duration-300 group-open:rotate-45">
-                                        +
-                                    </span>
-                                </summary>
-                                <p className="lg:text-base text-sm lg:w-[640px] w-full pb-6 text-[#AEAEAE] leading-6 font-medium">
-                                    {item.answer}
-                                </p>
-                            </details>
-                        )
-                    })}
-                    <Link href={`/${locale}/faq-help`} className='lg:hidden flex justify-center'>
-                        <button className='mt-6 bg-[#1C1C1C] text-sm text-[#fff] rounded-2xl px-6 py-4 font-semibold cursor-pointer'>
+        <BoxWrapped className="lg:py-20 lg:px-30 px-6 py-12 flex lg:flex-row flex-col justify-between mx-auto">
+            <div className="text-white flex lg:flex-col flex-row justify-between lg:h-[450px]">
+                <div>
+                    <h1 className="lg:text-6xl text-[32px] font-semibold">
+                        {t('title')}
+                    </h1>
+                    <p className="lg:text-base text-sm mt-3 text-[#AEAEAE]">
+                        {t('subtitle')}
+                    </p>
+                    <Link href={`/${locale}/faq-help`} className='lg:block hidden'>
+                        <button className='lg:mt-10 bg-[#1C1C1C] text-lg rounded-2xl px-6 py-4 font-semibold cursor-pointer'>
                             <p className="flex justify-center items-center gap-3"> {locale === 'en' ? 'View more' : '查看更多'} <Image src={arrow_right} alt='arrow' className="font-light group-hover:translate-x-1.5 transition ease-in-out animate-translateX" /></p>
                         </button>
                     </Link>
                 </div>
-            </BoxWrapped>
-        </div>
+                <Image src={message} alt="Message Icon" width={205} height={180} className='lg:w-full lg:h-[180px] w-[190px] h-[150px] object-cover object-bottom' />
+            </div>
+            <div className="text-white">
+                {t.raw("faqItems").map((item: FAQ, index: number) => {
+                    return (
+                        <details key={index} className="cursor-pointer focus:outline-none group border-b border-gray-800 ">
+                            <summary className="lg:w-[640px] w-full flex justify-between items-center text-left py-6">
+                                <p className="lg:text-2xl text-lg font-medium lg:w-full w-[90%]">{item.question}</p>
+                                <span className="text-3xl transition-transform duration-300 group-open:rotate-45">
+                                    +
+                                </span>
+                            </summary>
+                            <p className="lg:text-base text-sm lg:w-[640px] w-full pb-6 text-[#AEAEAE] leading-6 font-medium">
+                                {item.answer}
+                            </p>
+                        </details>
+                    )
+                })}
+                <Link href={`/${locale}/faq-help`} className='lg:hidden flex justify-center'>
+                    <button className='mt-6 bg-[#1C1C1C] text-sm text-[#fff] rounded-2xl px-6 py-4 font-semibold cursor-pointer'>
+                        <p className="flex justify-center items-center gap-3"> {locale === 'en' ? 'View more' : '查看更多'} <Image src={arrow_right} alt='arrow' className="font-light group-hover:translate-x-1.5 transition ease-in-out animate-translateX" /></p>
+                    </button>
+                </Link>
+            </div>
+        </BoxWrapped>
     )
 }
 
