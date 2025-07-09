959df0ac6dc0fddd5d8641726276dfe80e830809
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-07-10 00:32:40 +0700

    fix : term

--- src/components/term-condition/index.tsx
+++ src/components/term-condition/index.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useLocale } from 'next-intl';
 import Image from 'next/image';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { term as enBlog } from '@/i18n/messages/en.json';
@@ -9,7 +9,7 @@ import { term as ziBlog } from '@/i18n/messages/zi.json';
 import logo from '@/images/term/newpay_square_mark.svg';
 import TermContent from './term-content';
 import TermGeneral from './term-general';
-
+import bg_term from '@/images/faq-help/gradient.png';
 export default function TermsAndConditions() {
 
   const generalChildrenIds = [
@@ -49,13 +49,21 @@ export default function TermsAndConditions() {
     'general-4-4',
     'general-4-5',
   ]
+  const idPivacy = ['privacy-1', 'privacy-2', 'privacy-3', 'privacy-4', 'privacy-5', 'privacy-6', 'privacy-7', 'privacy-8', 'privacy-9', 'privacy-10', 'privacy-11', 'privacy-12', 'privacy-13', 'privacy-14']
+
+  const idCommission = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
+
+
   const [expandedSections, setExpandedSections] = useState<Record<string, boolean>>({
     'general': true,
-    'newpay-card': true
+    'privacy': true,
+    'commission': true
   });
 
   const [activeSection, setActiveSection] = useState<string>('general');
   const [displaySection, setDisplaySection] = useState<string>('general');
+  const [pendingScrollId, setPendingScrollId] = useState<string | null>(null);
+
 
   //toggle the expanded state of a section
   const toggleSection = (section: string) => {
@@ -67,21 +75,18 @@ export default function TermsAndConditions() {
 
   const handleSectionClick = (sectionId: string) => {
     setActiveSection(sectionId);
-
+  
     if (generalChildrenIds.includes(sectionId) || idSubitems.includes(sectionId)) {
       setDisplaySection('general');
+    } else if (idPivacy.includes(sectionId)) {
+      setDisplaySection('privacy');
+    } else if (idCommission.includes(sectionId)) {
+      setDisplaySection('commission');
     } else {
       setDisplaySection(sectionId);
     }
-
-    setTimeout(() => {
-      const el = document.getElementById(sectionId);
-      if (el) {
-        el.scrollIntoView({ behavior: 'smooth', block: 'end' });
-      }
-    }, 100);
+    setPendingScrollId(sectionId);
   };
-
 
   const locale = useLocale();
   const termData = locale === 'en' ? enBlog : ziBlog;
@@ -89,6 +94,24 @@ export default function TermsAndConditions() {
   const mainContent = termData.mainContent;
 
   const currentSectionContent = (mainContent as Record<string, any>)[displaySection];
+
+  useEffect(() => {
+    if (!pendingScrollId) return;
+  
+    const scroll = () => {
+      const el = document.getElementById(pendingScrollId);
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        setPendingScrollId(null);
+      } else {
+        setTimeout(scroll, 100);
+      }
+    };
+  
+    scroll();
+  }, [pendingScrollId]);
+  
+  
 
 
   // Render sidebar items recursively
@@ -144,22 +167,29 @@ export default function TermsAndConditions() {
   return (
     <div className="bg-black lg:pt-0 pt-30">
       {/* Header */}
-      <div className="text-center md:h-[400px] flex justify-center items-center">
-        <div className='m-auto'>
-          <Image src={logo} alt="NewPay Logo" className='mx-auto' />
-          <h1 style={{
-            background: "linear-gradient(93deg, #FFF 21.92%, rgba(255, 255, 255, 0.00) 149.05%)",
-            backgroundClip: "text",
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent",
-          }}
-            className="lg:text-7xl text-4xl font-semibold text-[#AEAEAE] pt-10">
-            {termData.header.title}
-          </h1>
+      <div style={{
+        backgroundImage: `url(${bg_term.src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '400px',
+      }} className='relative'>
+        <div className="absolute inset-0 text-center flex justify-center items-center">
+          <div className='m-auto'>
+            <Image src={logo} alt="NewPay Logo" className='mx-auto' />
+            <h1 style={{
+              background: "linear-gradient(93deg, #FFF 21.92%, rgba(255, 255, 255, 0.00) 149.05%)",
+              backgroundClip: "text",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+              className="lg:text-7xl text-4xl font-semibold text-[#AEAEAE] pt-10">
+              {termData.header.title}
+            </h1>
+          </div>
         </div>
       </div>
 
-      <div className="container lg:pt-0 lg:px-0 pt-8 mx-auto flex flex-col lg:flex-row gap-8 text-sm">
+      <div className="container lg:px-0 pt-8 mx-auto flex flex-col lg:flex-row gap-8 text-sm">
         {/* Sidebar */}
         <div className="lg:w-1/3 px-2 lg:px-0">
           <div className="rounded-lg overflow-hidden">
@@ -172,10 +202,10 @@ export default function TermsAndConditions() {
 
         {/* Main Content */}
         {currentSectionContent && (
-          currentSectionContent === termData.mainContent.general ? (
-            <TermGeneral currentSectionContent={currentSectionContent} ids={generalChildrenIds} idSubitems={idSubitems} />
+          displaySection === 'general' ? (
+            <TermGeneral currentSectionContent={currentSectionContent} ids={generalChildrenIds} idSubitems={idSubitems}/>
           ) : (
-            <TermContent content={currentSectionContent} />
+            <TermContent content={currentSectionContent} idPivacy={idPivacy} idCommission={idCommission}/>
           )
         )}
       </div>
