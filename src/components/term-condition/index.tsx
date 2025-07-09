2128316d96dfd632f2a9a0ae731131f74805d814
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-07-09 18:45:23 +0700

    fix : term-conditions

--- src/components/term-condition/index.tsx
+++ src/components/term-condition/index.tsx
@@ -1,25 +1,61 @@
 'use client'
 import { useLocale } from 'next-intl';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { RiArrowRightSLine } from "react-icons/ri";
-
 import { term as enBlog } from '@/i18n/messages/en.json';
 import { term as ziBlog } from '@/i18n/messages/zi.json';
-
 import logo from '@/images/term/newpay_square_mark.svg';
+import TermContent from './term-content';
+import TermGeneral from './term-general';
 
 export default function TermsAndConditions() {
 
-  // State to manage expanded sections
+  const generalChildrenIds = [
+    'general',
+    'general-1',
+    'general-2',
+    'general-3',
+    'general-4',
+    'general-5',
+    'general-6',
+    'general-7',
+    'general-8',
+    'general-9',
+    'general-10',
+    'general-11',
+    'general-12',
+    'general-13',
+    'general-14',
+  ]
+  const idSubitems = [
+    'general-1-1',
+    'general-1-2',
+    'general-1-3',
+    'general-1-4',
+    'general-2-1',
+    'general-2-2',
+    'general-2-3',
+    'general-2-4',
+    'general-3-1',
+    'general-3-2',
+    'general-3-3',
+    'general-3-4',
+    'general-3-5',
+    'general-4-1',
+    'general-4-2',
+    'general-4-3',
+    'general-4-4',
+    'general-4-5',
+  ]
   const [expandedSections, setExpandedSections] = useState<Record<string, boolean>>({
     'general': true,
     'newpay-card': true
   });
 
-  // State to manage the active section 
-  const [activeSection, setActiveSection] = useState<string>('hongkong-card');
+  const [activeSection, setActiveSection] = useState<string>('general');
+  const [displaySection, setDisplaySection] = useState<string>('general');
 
   //toggle the expanded state of a section
   const toggleSection = (section: string) => {
@@ -28,16 +64,32 @@ export default function TermsAndConditions() {
       [section]: !prev[section]
     }));
   };
-  //handle section click and set active section
-  const handleSectionClick = (section: string) => {
-    setActiveSection(section);
+
+  const handleSectionClick = (sectionId: string) => {
+    setActiveSection(sectionId);
+
+    if (generalChildrenIds.includes(sectionId) || idSubitems.includes(sectionId)) {
+      setDisplaySection('general');
+    } else {
+      setDisplaySection(sectionId);
+    }
+
+    setTimeout(() => {
+      const el = document.getElementById(sectionId);
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      }
+    }, 100);
   };
+
 
   const locale = useLocale();
   const termData = locale === 'en' ? enBlog : ziBlog;
   const sidebarItems = termData.sidebarItems;
   const mainContent = termData.mainContent;
-  const generalTerms = termData.mainContent.generalTerms;
+
+  const currentSectionContent = (mainContent as Record<string, any>)[displaySection];
+
 
   // Render sidebar items recursively
   const renderSidebarItem = (item: any, depth = 0) => {
@@ -55,13 +107,22 @@ export default function TermsAndConditions() {
               handleSectionClick(item.id);
             }
           }}
-          className={`w-full text-left ${item.isMain ? 'text-[#FFF] font-medium  py-4' : depth === 1
-            ? `px-4 py-3 mb-2 ${isActive ? 'text-white bg-[#FFFFFF]/4 rounded-lg' : 'text-[#AEAEAE] hover:text-[#AEAEAE] hover:bg-[#FFFFFF]/4 hover:rounded-lg '}`
-            : depth === 2 ? `ml-8 px-4 py-3 border-l-2 ${isActive ? 'text-[#E77228] border-[#E77228] font-semibold' : 'text-[#AEAEAE] hover:text-[#AEAEAE] hover:bg-[#FFFFFF]/4'}` : ''
-            }`}
+          className={`w-full text-left 
+    ${item.isMain ? 'text-[#FFF] font-medium p-4 my-1' : ''}
+    ${depth === 1 ? 'px-4 py-3 mb-2' : ''}
+    ${depth === 2 ? 'ml-8 px-4 py-3 border-l-2' : ''}
+    ${isActive
+              ? depth === 2
+                ? 'text-[#E77228] border-[#E77228] font-semibold bg-[#FFFFFF]/4'
+                : 'text-white bg-[#FFFFFF]/4 rounded-lg'
+              : depth === 2
+                ? 'text-[#AEAEAE] hover:text-[#AEAEAE] hover:bg-[#FFFFFF]/4'
+                : 'text-[#AEAEAE] hover:text-[#AEAEAE] hover:bg-[#FFFFFF]/4 hover:rounded-lg'
+            }
+  `}
         >
           <div className="flex items-center justify-between">
-            <span className=" text-sm">{item.label}</span>
+            <span className="text-sm">{item.label}</span>
             {item.hasChildren && (
               <div className="flex-shrink-0 ml-2">
                 {isExpanded ? <MdKeyboardArrowDown size={20} /> : <RiArrowRightSLine size={20} />}
@@ -81,7 +142,7 @@ export default function TermsAndConditions() {
   };
 
   return (
-    <div className="bg-black">
+    <div className="bg-black lg:pt-0 pt-30">
       {/* Header */}
       <div className="text-center md:h-[400px] flex justify-center items-center">
         <div className='m-auto'>
@@ -92,15 +153,15 @@ export default function TermsAndConditions() {
             WebkitBackgroundClip: "text",
             WebkitTextFillColor: "transparent",
           }}
-            className="text-7xl md:text-5xl font-semibold text-[#AEAEAE] pt-10">
+            className="lg:text-7xl text-4xl font-semibold text-[#AEAEAE] pt-10">
             {termData.header.title}
           </h1>
         </div>
       </div>
 
-      <div className="container mx-auto flex flex-col lg:flex-row gap-8 text-sm">
+      <div className="container lg:pt-0 lg:px-0 pt-8 mx-auto flex flex-col lg:flex-row gap-8 text-sm">
         {/* Sidebar */}
-        <div className="lg:w-1/3">
+        <div className="lg:w-1/3 px-2 lg:px-0">
           <div className="rounded-lg overflow-hidden">
             <nav className="space-y-0">
               {sidebarItems.map((item) => renderSidebarItem(item))}
@@ -110,62 +171,13 @@ export default function TermsAndConditions() {
         </div>
 
         {/* Main Content */}
-        <div className="lg:w-3/4">
-          <div className="bg-opacity-40 backdrop-blur-sm rounded-lg p-8">
-            <h2 className="text-3xl font-semibold text-[#FFF] mb-6">
-              {termData.mainContent.title}
-            </h2>
-
-            <p className="text-gray-300 text-sm mb-8">
-              {termData.mainContent.lastUpdated}
-            </p>
-
-            <div className="mb-8">
-              <h3 className="text-2xl font-semibold text-[#FFF] mb-4">
-                {mainContent.introSection.title}
-              </h3>
-
-              {mainContent.introSection.paragraphs.map(p => {
-                return (
-                  <p key={p}
-                    className="text-[#AEAEAE] mb-4 leading-relaxed text-sm">
-                    {p}
-                  </p>
-                );
-              })}
-            </div>
-
-            <div className="space-y-8">
-              <section>
-                <h3 className="text-xl font-semibold text-[#FFF] mb-4">
-                  {generalTerms.title}
-                </h3>
-
-                <div className="space-y-6">
-                  {
-                    generalTerms.sections.map((section, index) => (
-                      <div className="text-sm" key={index}>
-                        <h4 className="font-semibold text-[#FFF] mb-3">{section.title}</h4>
-                        {section.content.map((c, idx) => {
-                          return (
-                            <div key={idx}>
-                              <p className="text-[#AEAEAE] leading-relaxed mb-4">
-                                {c.paragraph}
-                              </p>
-                              {c.subItems && c.subItems.map((subItem, subInx) => (
-                                <p key={subInx} className="text-[#AEAEAE]">{subItem}</p>
-                              ))}
-                            </div>
-                          )
-                        })}
-                      </div>
-                    ))
-                  }
-                </div>
-              </section>
-            </div>
-          </div>
-        </div>
+        {currentSectionContent && (
+          currentSectionContent === termData.mainContent.general ? (
+            <TermGeneral currentSectionContent={currentSectionContent} ids={generalChildrenIds} idSubitems={idSubitems} />
+          ) : (
+            <TermContent content={currentSectionContent} />
+          )
+        )}
       </div>
     </div >
   );
