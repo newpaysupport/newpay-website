1a7e96809df5ebfc6c7b5cfcdd32f1709bd1c979
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2025-07-11 14:13:30 +0700

    fix: scroll term

--- src/components/term-condition/index.tsx
+++ src/components/term-condition/index.tsx
@@ -215,9 +215,18 @@ export default function TermsAndConditions() {
 
       <div className="container lg:px-20 pt-8 mx-auto flex flex-col lg:flex-row gap-8 text-sm">
         {/* Sidebar */}
-        <div className="lg:w-1/3 px-2 lg:px-0">
-          <div className="sticky top-18 max-h-[calc(100vh-72px)] overflow-y-auto rounded-lg custom-scroll scroll-smooth lg:sidebar-container">
+        {/* desktop */}
+        <div className="hidden lg:block lg:w-1/3 px-2 lg:px-0">
+          <div className="sticky top-18 max-h-[calc(100vh-72px)] overflow-y-auto rounded-lg custom-scroll scroll-smooth sidebar-container">
             <nav className="space-y-0 max-h-[calc(100vh-80px)]">
+              {sidebarItems.map((item) => renderSidebarItem(item))}
+            </nav>
+          </div>
+        </div>
+        {/* mobile */}
+        <div className="lg:hidden px-2 ">
+          <div className=" overflow-y-auto rounded-lg">
+            <nav className="space-y-0">
               {sidebarItems.map((item) => renderSidebarItem(item))}
             </nav>
           </div>
